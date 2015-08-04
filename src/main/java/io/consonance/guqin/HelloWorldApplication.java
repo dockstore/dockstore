c3d90a18dcaee356bb0443a550dbd1f8c2621884
Denis Yuen <dyuen@oicr.on.ca>
2015-08-04 11:06:46 -0400

    Saving work on prototype
    Functions with swagger

---
+++ src/main/java/io/consonance/guqin/HelloWorldApplication.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2015 Consonance
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.consonance.guqin;
+
+import io.consonance.guqin.resources.HelloWorldResource;
+import io.consonance.guqin.resources.TemplateHealthCheck;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+/**
+ *
+ * @author dyuen
+ */
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.2");
+        beanConfig.setSchemes(new String[] { "http" });
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/api");
+        beanConfig.setResourcePackage("io.consonance.guqin.resources");
+        beanConfig.setScan(true);
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+
+        final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(), configuration.getDefaultName());
+        environment.jersey().register(resource);
+
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+
+        // swagger stuff
+
+        // Swagger providers
+        environment.jersey().register(ApiListingResource.class);
+        environment.jersey().register(SwaggerSerializers.class);
+
+        // Swagger Scanner, which finds all the resources for @Api Annotations
+        // ScannerFactory.setScanner(new DefaultJaxrsScanner());
+
+        // // Add the reader, which scans the resources and extracts the resource information
+        // ClassReaders.setReader(new DefaultJaxrsApiReader());
+        //
+        // // Set the swagger config options
+        // SwaggerConfig config = ConfigFactory.config();
+        // config.setApiVersion("1.0.1");
+        // config.setBasePath("http://localhost:8000");
+
+        // optional CORS support
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+    }
+}
---
+++ src/main/java/io/consonance/guqin/resources/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Web resources
+ * @since 1.0
+ */
+package io.consonance.guqin.resources;
+
