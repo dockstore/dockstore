41ccad8ff9e73a71110365f630b4b9fc377d5f5b
Brian O'Connor <briandoconnor@gmail.com>
2015-09-19 17:39:33 -0400

    adding a dockerfile, work in progress

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM postgres:9.4
+
+RUN mkdir /gitroot
+COPY checkstyle.xml /gitroot/
+COPY dependency-reduced-pom.xml /gitroot/
+COPY findbugs-exclude.xml /gitroot/
+COPY pom.xml /gitroot/
+COPY docker-entrypoint.sh /gitroot/
+COPY src /gitroot/src
+
+# now build this
+RUN cd /gitroot/ && apt-get update && apt-get install -y maven
+RUN cd /gitroot && mvn clean install
+
+ENTRYPOINT ["/bin/sh", "-c"]
+
+CMD /gitroot/docker-entrypoint.sh
---
+++ docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Note: I've written this using sh so it works in the busybox container too
+
+# USE the trap if you need to also do manual cleanup after the service is stopped,
+#     or need to start multiple services in the one container
+trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+
+# start service in background here
+#/usr/sbin/apachectl start
+# the entrypoint provided by the base Postgres container
+/docker-entrypoint.sh &
+
+# todo put the web service startup here
+java -jar /gitroot/target/dockstore-*-SNAPSHOT.jar server /hello-world.yml
+
+echo "[hit enter key to exit] or run 'docker stop <container>'"
+read
+
+# stop service and clean up here
+echo "stopping postgres"
+#/usr/sbin/apachectl stop
+# not sure if this is right
+gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+
+echo "exited $0"
