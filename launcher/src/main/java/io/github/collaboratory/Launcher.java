2c3719a74350458fff535e107464035882c0402d
Brian O'Connor <briandoconnor@gmail.com>
2015-09-29 16:50:22 -0400

    adding files

--- launcher/src/main/java/io/github/collaboratory/Launcher.java
+++ launcher/src/main/java/io/github/collaboratory/Launcher.java
@@ -1,24 +1,27 @@
 package io.github.collaboratory;
 
 import org.apache.commons.cli.*;
-import org.apache.commons.cli.ParseException;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.HierarchicalINIConfiguration;
 import org.apache.commons.configuration.SubnodeConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
+import org.json.simple.parser.JSONParser;
 
 import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by boconnor on 9/24/15.
  */
 public class Launcher {
 
+    private Log log = null;
     Options options = null;
     CommandLineParser parser = null;
     CommandLine line = null;
@@ -26,6 +29,8 @@ public class Launcher {
     Object json = null;
 
     public Launcher(String [ ] args) {
+
+        log = LogFactory.getLog(this.getClass());
 
         // create the command line parser
         parser = setupCommandLineParser();
@@ -37,9 +42,8 @@ public class Launcher {
         config = getINIConfig(line.getOptionValue("config"));
 
         // now read the JSON file
+        // TODO: support comments in the JSON
         json = parseDescriptor(line.getOptionValue("descriptor"));
-
-        // LEFT OFF HERE!
 
         // setup directories
         setupDirectories(json);
@@ -48,10 +52,10 @@ public class Launcher {
         pullDockerImages(json);
 
         // pull data files
-        pullFiles("data", json);
+        pullFiles("DATA", json);
 
         // pull input files
-        pullFiles("inputs", json);
+        pullFiles("INPUT", json);
 
         // construct command
         constructCommand(json);
@@ -64,7 +68,18 @@ public class Launcher {
     }
 
     private void setupDirectories(Object json) {
-        //TODO
+
+        log.info("MAKING DIRECTORIES...");
+        // directory to use, typically a large, encrypted filesystem
+        String workingDir = config.getString("working-directory");
+        // make UUID
+        UUID uuid = UUID.randomUUID();
+        // setup directories
+        execute("mkdir -p "+workingDir+"/launcher-"+uuid.toString());
+        execute("mkdir -p "+workingDir+"/launcher-"+uuid.toString()+"/configs");
+        execute("mkdir -p "+workingDir+"/launcher-"+uuid.toString()+"/working");
+        execute("mkdir -p "+workingDir+"/launcher-"+uuid.toString()+"/inputs");
+        execute("mkdir -p "+workingDir+"/launcher-"+uuid.toString()+"/logs");
     }
 
     private void pushOutputFiles(Object json) {
@@ -80,12 +95,34 @@ public class Launcher {
     }
 
     private void pullFiles(String data, Object json) {
-        //TODO
+
+        log.info("DOWNLOADING "+data+" FILES...");
+
+        // just figure out which files from the JSON are we pulling
+        String type = null;
+        if ("DATA".equals(data)) {
+            type = "data";
+        } else if ("INPUT".equals(data)) {
+            type = "inputs";
+        }
+
+        // get list of files
+        JSONArray files = (JSONArray) ((JSONObject) json).get(type);
+        for (Object file : files) {
+
+            String filePath = (String) ((JSONObject) file).get("path");
+            String fileURL = (String) ((JSONObject) file).get("");
+
+            // imagesHash.put(image.toString(), image.toString());
+            log.info("FILE: ");
+
+        }
+
     }
 
     private void pullDockerImages(Object json) {
 
-        System.out.println("PULLING DOCKER CONTAINERS...");
+        log.info("PULLING DOCKER CONTAINERS...");
 
         // list of images to pull
         HashMap<String, String> imagesHash = new HashMap<String, String>();
@@ -101,7 +138,8 @@ public class Launcher {
 
         // pull the images
         for(String image : imagesHash.keySet()) {
-            System.out.println("CMD: docker pull " + image);
+            String command = "docker pull " + image;
+            execute(command);
         }
     }
 
@@ -109,6 +147,8 @@ public class Launcher {
 
         JSONParser parser=new JSONParser();
         Object obj = null;
+
+        // TODO: would be nice to support comments
 
         try {
             obj = parser.parse(new FileReader(new File(descriptorFile)));
@@ -145,7 +185,7 @@ public class Launcher {
                     String key = (it.next());
                     Object value = subConf.getString(key);
                     key = key.replace(doubleDot, dot);
-                    System.out.println("KEY: "+key+" VALUE: "+value);
+                    log.info("KEY: "+key+" VALUE: "+value);
                 }
             }
         } catch (FileNotFoundException e) {
@@ -165,7 +205,7 @@ public class Launcher {
 
         }
         catch( ParseException exp ) {
-            System.out.println( "Unexpected exception:" + exp.getMessage() );
+            log.error("Unexpected exception:" + exp.getMessage());
         }
 
         return(line);
@@ -182,6 +222,19 @@ public class Launcher {
         options.addOption( "d", "descriptor", true, "a JSON tool descriptor used to construct the command and run it" );
 
         return parser;
+    }
+
+    private void execute (String command) {
+        try {
+            log.info("CMD: "+command);
+            Runtime.getRuntime().exec(command).waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
     }
 
     public static void main(String [ ] args)
