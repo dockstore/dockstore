a5f3a4438d164a98f166bac79309d24d42b08e24
Brian O'Connor <briandoconnor@gmail.com>
2015-10-13 19:32:44 -0400

    cleaning up tests, now both CWL and regular

--- launcher/src/test/java/io/github/collaboratory/LauncherTest.java
+++ launcher/src/test/java/io/github/collaboratory/LauncherTest.java
@@ -16,22 +16,31 @@ public class LauncherTest {
         @Rule
         public ExpectedException expectedEx = ExpectedException.none();
 
-        // @Test
-        // public void testMain() throws Exception {
-        //         File iniFile = FileUtils.getFile("src", "test", "resources", "launcher.ini");
-        //         File jsonFile = FileUtils.getFile("src", "test", "resources", "collab.json");
-        //         expectedEx.expect(AmazonClientException.class);
-        //         expectedEx.expectMessage("Unable to load AWS credentials from any provider in the chain");
-        //         new Launcher(new String[] { "--config", iniFile.getAbsolutePath(), "--descriptor", jsonFile.getAbsolutePath() });
-        // }
+        @Test
+        public void testMain() throws Exception {
+                File iniFile = FileUtils.getFile("src", "test", "resources", "launcher.ini");
+                File jsonFile = FileUtils.getFile("src", "test", "resources", "collab.json");
+                if (System.getenv("AWS_ACCESS_KEY")  == null || System.getenv("AWS_SECRET_KEY") == null) {
+                    expectedEx.expect(AmazonClientException.class);
+                    expectedEx.expectMessage("Unable to load AWS credentials from any provider in the chain");
+                }
+                new Launcher(new String[] { "--config", iniFile.getAbsolutePath(), "--descriptor", jsonFile.getAbsolutePath() });
+
+        }
 
         @Test
         public void testCWL() throws Exception {
                 File iniFile = FileUtils.getFile("src", "test", "resources", "launcher.ini");
                 File cwlFile = FileUtils.getFile("src", "test", "resources", "collab.cwl");
-                File jobFile = FileUtils.getFile("src", "test", "resources", "collab-cwl-job.json");
+                File jobFile = FileUtils.getFile("src", "test", "resources", "collab-cwl-job-pre.json");
+
+            if (System.getenv("AWS_ACCESS_KEY") == null || System.getenv("AWS_SECRET_KEY") == null) {
+                    expectedEx.expect(AmazonClientException.class);
+                    expectedEx.expectMessage("Unable to load AWS credentials from any provider in the chain");
+                }
                 new LauncherCWL(new String[] { "--config", iniFile.getAbsolutePath(),
                                                "--descriptor", cwlFile.getAbsolutePath(),
                                                "--job", jobFile.getAbsolutePath()});
+
         }
 }
