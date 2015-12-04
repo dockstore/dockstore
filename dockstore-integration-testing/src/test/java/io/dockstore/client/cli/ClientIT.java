fff140a86dcb14bd25568cd913849585dfeb8385
victoroicr <XiangKun.Liu@oicr.on.ca>
2015-12-04 12:01:01 -0500

    9000 is used for ui, changed to 9001

--- dockstore-integration-testing/src/test/java/io/dockstore/client/cli/ClientIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/client/cli/ClientIT.java
@@ -16,6 +16,10 @@
  */
 package io.dockstore.client.cli;
 
+import static io.dockstore.common.CommonTestUtilities.DUMMY_TOKEN_1;
+import static io.dockstore.common.CommonTestUtilities.clearState;
+import static io.dockstore.common.CommonTestUtilities.getTestingPostgres;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -31,17 +35,13 @@ import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import com.google.common.io.Files;
 
+import io.dockstore.common.CommonTestUtilities.TestingPostgres;
 import io.dockstore.webservice.DockstoreWebserviceApplication;
 import io.dockstore.webservice.DockstoreWebserviceConfiguration;
 import io.dockstore.webservice.core.Registry;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import io.swagger.client.ApiException;
-
-import static io.dockstore.common.CommonTestUtilities.DUMMY_TOKEN_1;
-import static io.dockstore.common.CommonTestUtilities.TestingPostgres;
-import static io.dockstore.common.CommonTestUtilities.clearState;
-import static io.dockstore.common.CommonTestUtilities.getTestingPostgres;
 
 /**
  *
@@ -65,103 +65,108 @@ public class ClientIT {
         return getConfigFileLocation(correctUser, true);
     }
 
-
     public String getConfigFileLocation(boolean correctUser, boolean validPort) throws IOException {
         File tempDir = Files.createTempDir();
         final File tempFile = File.createTempFile("config", "config", tempDir);
-        FileUtils.write(tempFile,"token: "+(correctUser? DUMMY_TOKEN_1 : "foobar") + "\n");
-        FileUtils.write(tempFile,"server-url: http://localhost:"+(validPort ? "8000":"9000")+"\n", true);
+        FileUtils.write(tempFile, "token: " + (correctUser ? DUMMY_TOKEN_1 : "foobar") + "\n");
+        FileUtils.write(tempFile, "server-url: http://localhost:" + (validPort ? "8000" : "9001") + "\n", true);
 
         return tempFile.getAbsolutePath();
     }
 
     @Test
     public void testListEntries() throws IOException, TimeoutException, ApiException {
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"list"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "list" });
     }
 
     @Test
     public void testDebugModeListEntries() throws IOException, TimeoutException, ApiException {
-        Client.main(new String[]{"--debug", "--config",getConfigFileLocation(true),"list"});
+        Client.main(new String[] { "--debug", "--config", getConfigFileLocation(true), "list" });
     }
 
     @Test
     public void testListEntriesWithoutCreds() throws IOException, TimeoutException, ApiException {
         systemExit.expectSystemExitWithStatus(1);
-        Client.main(new String[]{"--config",getConfigFileLocation(false),"list"});
+        Client.main(new String[] { "--config", getConfigFileLocation(false), "list" });
     }
 
     @Test
     public void testListEntriesOnWrongPort() throws IOException, TimeoutException, ApiException {
         systemExit.expectSystemExitWithStatus(Client.CONNECTION_ERROR);
-        Client.main(new String[]{"--config",getConfigFileLocation(true, false),"list"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true, false), "list" });
     }
 
     @Test
     public void quickRegisterValidEntry() throws IOException {
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"publish","quay.io/test_org/test6"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "publish", "quay.io/test_org/test6" });
 
         // verify DB
         final TestingPostgres testingPostgres = getTestingPostgres();
-        final long count = testingPostgres
-                               .runSelectStatement("select count(*) from container where name = 'test6'", new ScalarHandler<>());
-        Assert.assertTrue("should see three entries",count == 1);
+        final long count = testingPostgres.runSelectStatement("select count(*) from container where name = 'test6'", new ScalarHandler<>());
+        Assert.assertTrue("should see three entries", count == 1);
     }
 
     @Test
     public void quickRegisterDuplicateEntry() throws IOException {
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"publish","quay.io/test_org/test6"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"publish","quay.io/test_org/test6","view1"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"publish","quay.io/test_org/test6","view2"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "publish", "quay.io/test_org/test6" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "publish", "quay.io/test_org/test6", "view1" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "publish", "quay.io/test_org/test6", "view2" });
 
         // verify DB
         final TestingPostgres testingPostgres = getTestingPostgres();
-        final long count = testingPostgres
-                               .runSelectStatement("select count(*) from container where name = 'test6'", new ScalarHandler<>());
-        Assert.assertTrue("should see three entries",count == 3);
+        final long count = testingPostgres.runSelectStatement("select count(*) from container where name = 'test6'", new ScalarHandler<>());
+        Assert.assertTrue("should see three entries", count == 3);
     }
 
     @Test
     public void quickRegisterInValidEntry() throws IOException {
         systemExit.expectSystemExitWithStatus(Client.GENERIC_ERROR);
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"publish","quay.io/test_org/test1"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "publish", "quay.io/test_org/test1" });
     }
 
     @Test
     public void quickRegisterUnknownEntry() throws IOException {
         systemExit.expectSystemExitWithStatus(Client.GENERIC_ERROR);
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"publish","quay.io/funky_container_that_does_not_exist"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "publish", "quay.io/funky_container_that_does_not_exist" });
     }
 
     @Test
     public void manualRegisterABunchOfValidEntries() throws IOException {
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.QUAY_IO.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.QUAY_IO.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master", "--toolname", "test1"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.QUAY_IO.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master", "--toolname", "test2"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.DOCKER_HUB.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.DOCKER_HUB.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master", "--toolname", "test1"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.QUAY_IO.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.QUAY_IO.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master", "--toolname", "test1" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.QUAY_IO.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master", "--toolname", "test2" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.DOCKER_HUB.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.DOCKER_HUB.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master", "--toolname", "test1" });
 
         // verify DB
         final TestingPostgres testingPostgres = getTestingPostgres();
-        final long count = testingPostgres
-                               .runSelectStatement("select count(*) from container where name = 'bd2k-python-lib'", new ScalarHandler<>());
-        Assert.assertTrue("should see three entries",count == 5);
+        final long count = testingPostgres.runSelectStatement("select count(*) from container where name = 'bd2k-python-lib'",
+                new ScalarHandler<>());
+        Assert.assertTrue("should see three entries", count == 5);
     }
 
     @Test
     public void manualRegisterADuplicate() throws IOException {
         systemExit.expectSystemExitWithStatus(Client.GENERIC_ERROR);
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.QUAY_IO.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.QUAY_IO.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master", "--toolname", "test1"});
-        Client.main(new String[]{"--config",getConfigFileLocation(true),"manual_publish","--registry", Registry.QUAY_IO.toString(), "--namespace",
-            "pypi",  "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference", "refs/head/master", "--toolname", "test1"});
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.QUAY_IO.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.QUAY_IO.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master", "--toolname", "test1" });
+        Client.main(new String[] { "--config", getConfigFileLocation(true), "manual_publish", "--registry", Registry.QUAY_IO.toString(),
+                "--namespace", "pypi", "--name", "bd2k-python-lib", "--git-url", "git@github.com:funky-user/test2.git", "--git-reference",
+                "refs/head/master", "--toolname", "test1" });
     }
 
 }
