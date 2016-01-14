14652029800d1e161a6f0501b94bdd2ea02c1b13
Andrew Duncan <agduncan@uwaterloo.ca>
2016-01-14 15:01:35 -0500

    update secret archive

--- encrypt.sh
+++ encrypt.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-tar cvf dockstore-integration-testing/src/test/resources/secrets.tar dockstore-integration-testing/src/test/resources/config_file.txt dockstore-integration-testing/src/test/resources/db_dump.sql dockstore-integration-testing/src/test/resources/dockstoreTest.yml
+tar cvf dockstore-integration-testing/src/test/resources/secrets.tar dockstore-integration-testing/src/test/resources/config_file.txt dockstore-integration-testing/src/test/resources/db_confidential_dump.sql dockstore-integration-testing/src/test/resources/dockstoreTest.yml
 travis encrypt-file dockstore-integration-testing/src/test/resources/secrets.tar
 mv secrets.tar.enc dockstore-integration-testing/src/test/resources/secrets.tar.enc
 git add dockstore-integration-testing/src/test/resources/secrets.tar.enc
