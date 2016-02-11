33ff648c3c6c35c4ce7764ac5e04b5c8eebef8cc
Denis Yuen <denis.yuen@oicr.on.ca>
2016-02-11 14:17:10 -0500

    Cromwell compiles plus license and stub

---
+++ dockstore-client/src/main/java/io/dockstore/client/cli/WDlExperiment.java
@@ -0,0 +1,16 @@
+package io.dockstore.client.cli;
+
+import cromwell.Main;
+
+/**
+ * Stub to demonstrate that we can call Cromwell
+ * @author dyuen
+ */
+public class WDlExperiment {
+
+    public static void main(String[] args){
+
+        Main main = new Main();
+        main.usageAndExit();
+    }
+}
