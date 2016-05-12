b6deb6635fd2900cdc1ff240a791a297bd3c711c
Janice Patricia <janice.patricia@oicr.on.ca>
2016-05-12 13:31:00 -0400

    See if this fixes Travis-CI abnormal VM termination

--- dockstore-client/src/test/java/io/dockstore/client/cli/UpgradeTestIT.java
+++ dockstore-client/src/test/java/io/dockstore/client/cli/UpgradeTestIT.java
@@ -21,7 +21,9 @@ import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.google.common.io.Resources;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import java.io.IOException;
 import java.net.URL;
@@ -36,6 +38,9 @@ import static org.mockito.Mockito.when;
 
 public class UpgradeTestIT {
     private ObjectMapper objectMapper;
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
     @Before
     public void setup() throws IOException{
@@ -81,6 +86,7 @@ public class UpgradeTestIT {
 
     @Test
     public void upgradeTest() throws IOException {
+        exit.expectSystemExit();
         //if the current is newer and unstable, output "--upgrade-stable" command option
         //else if current is the latest stable version, output "you are currently running the most stable version"
         //         and option to "--upgrade-unstable"
@@ -91,6 +97,7 @@ public class UpgradeTestIT {
 
     @Test
     public void upgradeStable(){
+        exit.expectSystemExit();
         //if the current is unstable, upgrade to the latest stable version
         //else, output "you are currently running the most stable version" and option to "--upgrade-unstable"
         Client.main(new String[] {"--upgrade-stable"});
@@ -98,6 +105,7 @@ public class UpgradeTestIT {
 
     @Test
     public void upgradeUnstable(){
+        exit.expectSystemExit();
         //if the current is stable, upgrade to the most unstable version
         //else, output "you are currently running on the latest unstable version" and option to "--upgrade-stable"
         Client.main(new String[] {"--upgrade-unstable"});
