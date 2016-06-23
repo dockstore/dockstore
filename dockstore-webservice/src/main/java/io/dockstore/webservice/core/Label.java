a5f6e9bd2de9f3349f98dec18a0771cd0f80f53b
Denis Yuen <denis.yuen@oicr.on.ca>
2016-06-23 11:31:21 -0400

    Hook up VersionEye

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/Label.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/Label.java
@@ -23,7 +23,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Objects;
