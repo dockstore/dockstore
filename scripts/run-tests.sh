6e2e5347fd03a8550e594f01d7f81f4daffdf6b8
Denis Yuen <denis.yuen@oicr.on.ca>
2016-11-16 10:47:31 -0500

    Use || properly

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,7 @@ if [ ${#EXTRA_MAVEN_VAR} -eq 0 ]; then
 	mvn -B clean install -DskipITs=false -Pconfidential-tests ${EXTRA_MAVEN_VAR}
 else
         # for coverage builds, we need to be more picky, let's exclude builds from branches other than develop and master
-        if [ "${TRAVIS_BRANCH}" = "master" -a  "${TRAVIS_BRANCH}" = "develop" ]; then
+        if [ "${TRAVIS_BRANCH}" = "master" ] || [ "${TRAVIS_BRANCH}" = "develop" ] || [ "${TRAVIS_EVENT_TYPE}" = "push"   ]; then
 		mvn -B clean install -DskipITs=false -Pconfidential-tests ${EXTRA_MAVEN_VAR}
         fi
 fi
