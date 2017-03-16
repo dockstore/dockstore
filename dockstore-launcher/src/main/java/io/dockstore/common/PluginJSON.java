b3ae6d66aa8cce4f3de1bd3e8bc50baae189aec5
garyluu <garylau.work@gmail.com>
2017-03-16 10:42:07 -0400

    Removing setters for JSON classes

--- dockstore-launcher/src/main/java/io/dockstore/common/PluginJSON.java
+++ dockstore-launcher/src/main/java/io/dockstore/common/PluginJSON.java
@@ -24,23 +24,11 @@ public class PluginJSON {
         return version;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getLocation() {
         return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
     }
 }
