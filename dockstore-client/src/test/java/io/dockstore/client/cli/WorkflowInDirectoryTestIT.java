ee4995d6cb46e0060e79eea8aa82e42245e33a99
garyluu <garylau.work@gmail.com>
2017-08-04 15:40:27 -0400

    Adding another test

--- dockstore-client/src/test/java/io/dockstore/client/cli/WorkflowInDirectoryTestIT.java
+++ dockstore-client/src/test/java/io/dockstore/client/cli/WorkflowInDirectoryTestIT.java
@@ -14,8 +14,8 @@ public class WorkflowInDirectoryTestIT {
     @Test
     public void testWorkflowRunInDirectory() {
         Client client = new Client();
-        File cwlFile = new File(ResourceHelpers.resourceFilePath("workflow_in_directory/directory/1st-workflow.cwl"));
-        File cwlJSON = new File(ResourceHelpers.resourceFilePath("workflow_in_directory/directory/1st-workflow-job.yml"));
+        File cwlFile = new File(ResourceHelpers.resourceFilePath("testDirectory2/1st-workflow.cwl"));
+        File cwlJSON = new File(ResourceHelpers.resourceFilePath("testDirectory2/1st-workflow-job.yml"));
         File configFile = new File(ResourceHelpers.resourceFilePath("config"));
         ArrayList<String> args = new ArrayList<String>() {{
             add("--config");
@@ -26,6 +26,29 @@ public class WorkflowInDirectoryTestIT {
             add(cwlFile.getAbsolutePath());
             add("--yaml");
             add(cwlJSON.getAbsolutePath());
+        }};
+        client.main(args.toArray(new String[args.size()]));
+    }
+
+    @Test
+    public void testWorkflowRunInDirectory2() {
+        Client client = new Client();
+        File cwlFile = new File(ResourceHelpers.resourceFilePath("directory/1st-workflow.cwl"));
+        File cwlJSON = new File(ResourceHelpers.resourceFilePath("directory/1st-workflow-job.json"));
+        File configFile = new File(ResourceHelpers.resourceFilePath("config"));
+        String synapseCWLFile = "/home/gluu/synapse/NA12878-platinum-chr20-workflow/main-NA12878-platinum-chr20.cwl";
+        String synapseJSONFile = "/home/gluu/synapse/NA12878-platinum-chr20-workflow/main-NA12878-platinum-chr20-samples.json";
+        ArrayList<String> args = new ArrayList<String>() {{
+            add("--config");
+            add(configFile.getAbsolutePath());
+            add("workflow");
+            add("launch");
+            add("--local-entry");
+            add(cwlFile.getAbsolutePath());
+//            add(synapseCWLFile);
+            add("--yaml");
+            add(cwlJSON.getAbsolutePath());
+//            add(synapseJSONFile);
         }};
         client.main(args.toArray(new String[args.size()]));
     }
