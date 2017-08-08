2c86df305e8793f55a536bfbdad7b7db31a86c02
garyluu <garylau.work@gmail.com>
2017-08-08 12:01:58 -0400

    Added another test

--- dockstore-client/src/test/java/io/dockstore/client/cli/WorkflowInDirectoryTestIT.java
+++ dockstore-client/src/test/java/io/dockstore/client/cli/WorkflowInDirectoryTestIT.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 import io.dropwizard.testing.ResourceHelpers;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,12 +12,20 @@ import org.junit.Test;
  * @since 02/08/17
  */
 public class WorkflowInDirectoryTestIT {
+
+    private Client client;
+    private File configFile;
+
+    @Before
+    public void setup() {
+        client = new Client();
+        configFile = new File(ResourceHelpers.resourceFilePath("config"));
+    }
+
     @Test
     public void testWorkflowRunInDirectory() {
-        Client client = new Client();
         File cwlFile = new File(ResourceHelpers.resourceFilePath("testDirectory2/1st-workflow.cwl"));
         File cwlJSON = new File(ResourceHelpers.resourceFilePath("testDirectory2/1st-workflow-job.yml"));
-        File configFile = new File(ResourceHelpers.resourceFilePath("config"));
         ArrayList<String> args = new ArrayList<String>() {{
             add("--config");
             add(configFile.getAbsolutePath());
@@ -32,10 +41,8 @@ public class WorkflowInDirectoryTestIT {
 
     @Test
     public void testWorkflowRunInDirectory2() {
-        Client client = new Client();
         File cwlFile = new File(ResourceHelpers.resourceFilePath("directory/1st-workflow.cwl"));
         File cwlJSON = new File(ResourceHelpers.resourceFilePath("directory/1st-workflow-job.json"));
-        File configFile = new File(ResourceHelpers.resourceFilePath("config"));
         String synapseCWLFile = "/home/gluu/synapse/NA12878-platinum-chr20-workflow/main-NA12878-platinum-chr20.cwl";
         String synapseJSONFile = "/home/gluu/synapse/NA12878-platinum-chr20-workflow/main-NA12878-platinum-chr20-samples.json";
         ArrayList<String> args = new ArrayList<String>() {{
@@ -45,10 +52,27 @@ public class WorkflowInDirectoryTestIT {
             add("launch");
             add("--local-entry");
             add(cwlFile.getAbsolutePath());
-//            add(synapseCWLFile);
+            //            add(synapseCWLFile);
             add("--yaml");
             add(cwlJSON.getAbsolutePath());
-//            add(synapseJSONFile);
+            //            add(synapseJSONFile);
+        }};
+        client.main(args.toArray(new String[args.size()]));
+    }
+
+    @Test
+    public void testArrayOfArrayOfInputs() {
+        File cwlFile = new File(ResourceHelpers.resourceFilePath("arrayOfArrays/arrays.cwl"));
+        File cwlJSON = new File(ResourceHelpers.resourceFilePath("arrayOfArrays/testArrayLocalInputLocalOutput.json"));
+        ArrayList<String> args = new ArrayList<String>() {{
+            add("--config");
+            add(configFile.getAbsolutePath());
+            add("tool");
+            add("launch");
+            add("--local-entry");
+            add(cwlFile.getAbsolutePath());
+            add("--yaml");
+            add(cwlJSON.getAbsolutePath());
         }};
         client.main(args.toArray(new String[args.size()]));
     }
---
+++ dockstore-client/src/test/resources/arrayOfArrays/arrays.cwl
@@ -0,0 +1,71 @@
+#!/usr/bin/env cwl-runner
+
+class: CommandLineTool
+description: "Markdown description text here"
+id: "HelloWorld"
+label: "HelloWorld Tool"
+
+cwlVersion: v1.0
+
+dct:creator:
+  "@id": "http://orcid.org/0000-0003-3566-7705"
+  foaf:name: Peter Amstutz
+  foaf:mbox: "mailto:peter.amstutz@curoverse.com"
+
+requirements:
+  - class: DockerRequirement
+    dockerPull: "quay.io/collaboratory/dockstore-tool-linux-sort"
+  - class: InlineJavascriptRequirement
+
+hints:
+  - class: ResourceRequirement
+    coresMin: 8
+    ramMin: 8092
+    outdirMin: 512000
+    description: "these parameters are used to locate a VM with appropriate resources"
+
+inputs:
+  - id: "#ref_file_1"
+    type: File
+    description: "this describes a large reference file that does not change between runs"
+
+  - id: "#ref_file_2"
+    type: File
+    description: "this describes a large reference file that does not change between runs"
+
+  - id: "#hello_input"
+    type: File
+    description: "this describes an input file that should be provided before execution"
+
+  - id: "#arrayed_input"
+    type:
+      items:
+        items: File
+        type: array
+      type: array
+    description: "this demonstrates a workflow that takes an array of inputs, at least 2"
+
+outputs:
+  - id: "#hello_output"
+    type: File
+    outputBinding:
+      glob: hello-output.txt
+    description: "this describes an output file that should be saved after execution"
+
+  - id: "#wc_output"
+    type:
+      type: array
+      items: File
+    outputBinding:
+      glob: wc-output*.txt
+    description: "this describes an output file that should be saved after execution"
+
+
+baseCommand: ["bash", "-c"]
+arguments:
+  - valueFrom:
+        $("cat " + inputs.hello_input.path + " > hello-output.txt &&"
+            + " ls " + inputs.ref_file_1.path + " >> hello-output.txt && "
+            + " head -20 " + inputs.ref_file_2.path + " >> hello-output.txt && "
+            + " wc -l " + inputs.arrayed_input[0][0].path + " >> wc-output0.txt &&"
+            + " wc -l " + inputs.arrayed_input[0][1].path + " >> wc-output1.txt")
