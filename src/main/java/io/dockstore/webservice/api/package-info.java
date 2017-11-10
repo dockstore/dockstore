05e6fcffcedbfedfa4cdc35225a73cd0d5e032ca
Denis Yuen <denis.yuen@oicr.on.ca>
2017-11-10 15:06:04 -0500

    This stuff seems un-needed for gh-pages as well (#1024)

--- src/main/java/io/dockstore/webservice/api/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Model objects
- * @since 1.0
- */
-package io.dockstore.webservice.api;
-
--- src/main/java/io/dockstore/webservice/jdbi/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Data access objects.
- * @since 1.0
- */
-package io.dockstore.webservice.jdbi;
-
