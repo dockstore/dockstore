0c02918aba295d7ac5881ed32481d148dc728440
Denis Yuen <denis.yuen@oicr.on.ca>
2017-11-20 10:48:49 -0500

    Alter schema
    * update secret archive
    * Use new encrypted bundle
    * Add test enforcing that migration looks sane

--- encrypt.sh
+++ encrypt.sh
@@ -10,8 +10,9 @@ tar cvf dockstore-integration-testing/src/test/resources/secrets.tar dockstore-i
 GIT_DIR=`pwd`
 # execute always in the same place to keep generated variable names consistent (sigh)
 # go to a nested dir for the new db testing to maintain a consistent variable name
-mkdir -p /tmp/new_test_default
-cd /tmp/new_test_default
+CUSTOM_DIR_NAME=db_with_changelog
+mkdir -p /tmp/$CUSTOM_DIR_NAME
+cd /tmp/$CUSTOM_DIR_NAME
 travis encrypt-file $GIT_DIR/dockstore-integration-testing/src/test/resources/secrets.tar -r ga4gh/dockstore
 # copy the new file
 cp secrets.tar.enc $GIT_DIR/dockstore-integration-testing/src/test/resources
