b2beb4f7f9fbd79aab49b1b4c6ca6619a9b0ea6e
garyluu <garylau.work@gmail.com>
2018-01-19 14:28:14 -0500

    Expand test to include verified sources and verified checks

--- dockstore-webservice/src/test/java/io/swagger/api/impl/ToolsImplCommonTest.java
+++ dockstore-webservice/src/test/java/io/swagger/api/impl/ToolsImplCommonTest.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.google.common.collect.Table;
+import com.google.gson.Gson;
 import io.dockstore.common.SourceControl;
 import io.dockstore.webservice.DockstoreWebserviceConfiguration;
 import io.dockstore.webservice.core.SourceFile;
@@ -26,6 +27,11 @@ import static org.junit.Assert.assertEquals;
  * @since 17/01/18
  */
 public class ToolsImplCommonTest {
+    static final Gson gson = new Gson();
+    static final String REFERENCE1 = "aaa";
+    static final String REFERENCE2 = "bbb";
+    static final String REFERENCE3 = "ccc";
+    static String json;
     @Test
     public void sourceFileToToolDescriptor() throws Exception {
         SourceFile sourceFile = new SourceFile();
@@ -43,7 +49,10 @@ public class ToolsImplCommonTest {
 
     /**
      * This tests if the Dockstore Entry can be properly converted to a GA4GH Tool
-     *
+     * The Dockstore Entry is actually a Dockstore Workflow with 3 WorkflowVersions
+     * One workflow version is hidden and not verified
+     * The other two versions are not hidden and verified
+     * Those two versions are verified by separate groups
      * @throws Exception
      */
     @Test
@@ -56,19 +65,33 @@ public class ToolsImplCommonTest {
         WorkflowVersion actualWorkflowVersion1 = new WorkflowVersion();
         actualWorkflowVersion1.setWorkflowPath("/pcawg-cgp-somatic-workflow.wdl");
         actualWorkflowVersion1.setLastModified(null);
-        actualWorkflowVersion1.setReference("ccc");
+        actualWorkflowVersion1.setReference(REFERENCE1);
         actualWorkflowVersion1.addSourceFile(actualSourceFile1);
         actualWorkflowVersion1.setHidden(true);
         actualWorkflowVersion1.setValid(true);
-        actualWorkflowVersion1.setName("ccc");
+        actualWorkflowVersion1.setName(REFERENCE1);
         actualWorkflowVersion1.setDirtyBit(false);
         actualWorkflowVersion1.setValid(false);
         actualWorkflowVersion1.setVerifiedSource(null);
-        WorkflowVersion actualWorkflowVersion2 = actualWorkflowVersion1;
-        actualWorkflowVersion2.setName("master");
-        actualWorkflowVersion2.setReference("master");
-        actualWorkflowVersion2.setHidden(false);
         Workflow workflow = new Workflow();
+
+
+        json = gson.toJson(actualWorkflowVersion1);
+        WorkflowVersion actualWorkflowVersion2 = gson.fromJson(json, WorkflowVersion.class);
+        actualWorkflowVersion2.setName(REFERENCE2);
+        actualWorkflowVersion2.setReference(REFERENCE2);
+        actualWorkflowVersion2.setHidden(false);
+        actualWorkflowVersion2.setVerifiedSource("potatoTesterSource");
+        actualWorkflowVersion2.setVerified(true);
+
+        json = gson.toJson(actualWorkflowVersion2);
+        WorkflowVersion actualWorkflowVersion3 = gson.fromJson(json, WorkflowVersion.class);
+        actualWorkflowVersion3.setName(REFERENCE3);
+        actualWorkflowVersion3.setReference(REFERENCE3);
+        actualWorkflowVersion3.setVerifiedSource("chickenTesterSource");
+        workflow.addWorkflowVersion(actualWorkflowVersion1);
+        workflow.addWorkflowVersion(actualWorkflowVersion2);
+        workflow.addWorkflowVersion(actualWorkflowVersion3);
         workflow.setMode(WorkflowMode.FULL);
         workflow.setWorkflowName(null);
         workflow.setOrganization("ICGC-TCGA-PanCancer");
@@ -78,15 +101,13 @@ public class ToolsImplCommonTest {
         workflow.setDescriptorType("wdl");
         workflow.setDefaultWorkflowPath("/pcawg-cgp-somatic-workflow.wdl");
         workflow.setDefaultTestParameterFilePath(null);
-        workflow.addWorkflowVersion(actualWorkflowVersion1);
-        workflow.addWorkflowVersion(actualWorkflowVersion2);
         workflow.setId(950);
         workflow.setAuthor(null);
         workflow.setDescription(null);
         workflow.setLabels(Collections.emptySortedSet());
         workflow.setUsers(Collections.emptySet());
         workflow.setEmail(null);
-        workflow.setDefaultVersion("master");
+        workflow.setDefaultVersion(REFERENCE2);
         workflow.setIsPublished(true);
         workflow.setLastModified(null);
         workflow.setLastUpdated(null);
@@ -97,22 +118,29 @@ public class ToolsImplCommonTest {
         actualConfig.setScheme("http");
         Pair<Tool, Table<String, SourceFile.FileType, Object>> toolTablePair = ToolsImplCommon.convertEntryToTool(workflow, actualConfig);
         Tool actualTool = toolTablePair.getLeft();
-        ToolVersion expectedToolVersion = new ToolVersion();
-        expectedToolVersion.setName("master");
-        expectedToolVersion
-                .setUrl("http://localhost:8080/api/ga4gh/v2/tools/%23workflow%2Fgithub.com%2FICGC-TCGA-PanCancer%2Fwdl-pcawg-sanger-cgp-workflow/versions/master");
-        expectedToolVersion.setId("#workflow/github.com/ICGC-TCGA-PanCancer/wdl-pcawg-sanger-cgp-workflow:master");
+        ToolVersion expectedToolVersion1 = new ToolVersion();
+        expectedToolVersion1.setName(REFERENCE2);
+        expectedToolVersion1
+                .setUrl("http://localhost:8080/api/ga4gh/v2/tools/%23workflow%2Fgithub.com%2FICGC-TCGA-PanCancer%2Fwdl-pcawg-sanger-cgp-workflow/versions/"+ REFERENCE2);
+        expectedToolVersion1.setId("#workflow/github.com/ICGC-TCGA-PanCancer/wdl-pcawg-sanger-cgp-workflow:" + REFERENCE2);
         List<ToolVersion.DescriptorTypeEnum> descriptorTypeList = new ArrayList<>();
         descriptorTypeList.add(ToolVersion.DescriptorTypeEnum.WDL);
-        expectedToolVersion.setImage("");
-        expectedToolVersion.setDescriptorType(descriptorTypeList);
-        expectedToolVersion.setDockerfile(false);
+        expectedToolVersion1.setImage("");
+        expectedToolVersion1.setDescriptorType(descriptorTypeList);
+        expectedToolVersion1.setDockerfile(false);
         // Meta-version dates are currently dependant on the environment, disabling for now
-        expectedToolVersion.setMetaVersion(null);
-        expectedToolVersion.setVerified(false);
-        expectedToolVersion.setVerifiedSource("");
+        expectedToolVersion1.setMetaVersion(null);
+        expectedToolVersion1.setVerified(true);
+        expectedToolVersion1.setVerifiedSource("potatoTesterSource");
+        json = gson.toJson(expectedToolVersion1);
+        ToolVersion expectedToolVersion2 = gson.fromJson(json, ToolVersion.class);
+        expectedToolVersion2.setName(REFERENCE3);
+        expectedToolVersion2.setVerifiedSource("chickenTesterSource");
+        expectedToolVersion2.setUrl("http://localhost:8080/api/ga4gh/v2/tools/%23workflow%2Fgithub.com%2FICGC-TCGA-PanCancer%2Fwdl-pcawg-sanger-cgp-workflow/versions/" + REFERENCE3);
+        expectedToolVersion2.setId("#workflow/github.com/ICGC-TCGA-PanCancer/wdl-pcawg-sanger-cgp-workflow:"+ REFERENCE3);
         List<ToolVersion> expectedToolVersions = new ArrayList<>();
-        expectedToolVersions.add(expectedToolVersion);
+        expectedToolVersions.add(expectedToolVersion1);
+        expectedToolVersions.add(expectedToolVersion2);
         Tool expectedTool = new Tool();
         expectedTool
                 .setUrl("http://localhost:8080/api/ga4gh/v2/tools/%23workflow%2Fgithub.com%2FICGC-TCGA-PanCancer%2Fwdl-pcawg-sanger-cgp-workflow");
@@ -125,8 +153,8 @@ public class ToolsImplCommonTest {
         // Meta-version dates are currently dependant on the environment, disabling for now
         expectedTool.setMetaVersion(null);
         expectedTool.setContains(Collections.EMPTY_LIST);
-        expectedTool.setVerified(false);
-        expectedTool.setVerifiedSource("[]");
+        expectedTool.setVerified(true);
+        expectedTool.setVerifiedSource("[\"chickenTesterSource\",\"potatoTesterSource\"]");
         expectedTool.setSigned(false);
         expectedTool.setVersions(expectedToolVersions);
         actualTool.setMetaVersion(null);
@@ -135,8 +163,7 @@ public class ToolsImplCommonTest {
         List<ToolVersion> versions = actualTool.getVersions();
         versions.stream().forEach(version -> version.setMetaVersion(null));
         actualTool.setVersions(versions);
-
-        Assert.assertEquals(actualTool, expectedTool);
+        Assert.assertEquals(expectedTool, actualTool);
     }
 
     @Test
