cbffc1380449d35b0ac750bfce9e6a0433f1181a
garyluu <garylau.work@gmail.com>
2018-05-17 09:49:27 -0400

    update secret archive

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -7,8 +7,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-# should be for integration tests only
 if [ "${TESTING_PROFILE}" = "integration-tests" ] || [ "${TESTING_PROFILE}" = "toil-integration-tests" ]; then
-    openssl aes-256-cbc -K $encrypted_7e2d6ff95e70_key -iv $encrypted_7e2d6ff95e70_iv -in secrets.tar.enc -out secrets.tar -d
+    openssl aes-256-cbc -K $encrypted_6006c24f51c8_key -iv $encrypted_6006c24f51c8_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
 fi
