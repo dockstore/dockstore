907f342cc6e0846766613b717eb07015c9fd47b3
Abraham Chavez <achave11@ucsc.edu>
2018-06-12 10:26:09 -0700

    Removed compile step from maven build in pom file.
    New version of openapi.yaml which deletes the NIHdatacommons tag that was
    being excessively generated.
    
    Reverted swagger to original, modified version and termsOfService manually to avoid
    rebuild.
    
    Fixed shell script to not fail if the curl request does not go through.

--- dockstore-webservice/src/main/resources/swagger2open.sh
+++ dockstore-webservice/src/main/resources/swagger2open.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # Using https://mermade.org.uk/openapi-converter, generate openapi schema sources.
-if curl --output /dev/null --silent --head --fail 'https://mermade.org.uk/api/v1/status'; then
-  curl -F "filename=@src/main/resources/swagger.json" https://mermade.org.uk/api/v1/convert -o src/main/resources/open_api.yaml
+if curl -F "filename=@src/main/resources/swagger.json" https://mermade.org.uk/api/v1/convert -o src/main/resources/open_api.yaml; then
+  # Parse file to see if a bad response was returned from server. 'Does not disturb maven build'
+  if grep -q '<title>Error</title>' src/main/resources/open_api.yaml; then
+    echo "Bad response from server! Exiting without errors"
+    exit 0
+  fi
   # Parse out position variable not compatible with open-api schemas
   sed '/position/d;s/\<html><body><pre>//;w src/main/resources/openapi1.yaml' src/main/resources/open_api.yaml
   # Aggregate correct url for dockstore api. Using pipe instead of forward slash, avoid dealing with scape chars
