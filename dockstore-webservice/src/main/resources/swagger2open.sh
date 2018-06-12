728708551ebc9f83a1bfb2038ffef984ca9834ce
Abraham Chavez <achave11@ucsc.edu>
2018-06-12 14:39:36 -0700

    Modified script to now fail maven build if something goes wrong with the curl request. Removed json swagger generator.

--- dockstore-webservice/src/main/resources/swagger2open.sh
+++ dockstore-webservice/src/main/resources/swagger2open.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 # Using https://mermade.org.uk/openapi-converter, generate openapi schema sources.
-if curl -F "filename=@src/main/resources/swagger.json" https://mermade.org.uk/api/v1/convert -o src/main/resources/open_api.yaml; then
-  # Parse file to see if a bad response was returned from server. 'Does not disturb maven build'
-  if grep -q '<title>Error</title>' src/main/resources/open_api.yaml; then
-    echo "Bad response from server! Exiting without errors"
-    exit 0
-  fi
+STATE=`curl -F "filename=@src/main/resources/swagger.yaml" https://mermade.org.uk/api/v1/convert -o src/main/resources/open_api.yaml -w "%{http_code}"`
+eval "$STATE"
+if [ \( "$?" = 0 \) -o \(  "$STATE" = "200" \) ]; then
   # Parse out position variable not compatible with open-api schemas
   sed '/position/d;s/\<html><body><pre>//;w src/main/resources/openapi1.yaml' src/main/resources/open_api.yaml
   # Aggregate correct url for dockstore api. Using pipe instead of forward slash, avoid dealing with scape chars
@@ -18,7 +15,7 @@ if curl -F "filename=@src/main/resources/swagger.json" https://mermade.org.uk/ap
   ' src/main/resources/openapi2.yaml > src/main/resources/openapi.yaml
 
   # Clean directory from excess files.
-  rm src/main/resources/swagger.json src/main/resources/open_api.yaml src/main/resources/openapi1.yaml src/main/resources/openapi2.yaml
+  rm src/main/resources/open_api.yaml src/main/resources/openapi1.yaml src/main/resources/openapi2.yaml
 else
   echo "URL not available, graciously terminate"
 fi
