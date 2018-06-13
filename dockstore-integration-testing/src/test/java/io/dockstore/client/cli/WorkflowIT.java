c401238f82bd0f1a82fd013eb1aa84cf43325a61
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-06-13 15:41:12 -0400

    Feature/1449/checker launch auth (#1478)
    * conditional auth for checker endpoints
    * Add a convenience script for expanding our confidential bundle
    * added tests for checker workflow launch and download

--- dockstore-integration-testing/src/test/java/io/dockstore/client/cli/WorkflowIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/client/cli/WorkflowIT.java
@@ -247,12 +247,78 @@ public class WorkflowIT extends BaseIT {
 
         // should be able to launch properly with correct credentials even though the workflow is not published
         FileUtils.writeStringToFile(new File("md5sum.input"), "foo" , StandardCharsets.UTF_8);
-        systemExit.expectSystemExitWithStatus(0);
         Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "workflow", "launch", "--entry", toolpath, "--json" , ResourceHelpers.resourceFilePath("md5sum-wrapper-tool.json") ,  "--script" });
 
         // should not be able to launch properly with incorrect credentials
         systemExit.expectSystemExitWithStatus(Client.ENTRY_NOT_FOUND);
         Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file.txt"), "workflow", "launch", "--entry", toolpath, "--json" , ResourceHelpers.resourceFilePath("md5sum-wrapper-tool.json") ,  "--script" });
+    }
+
+    @Test
+    public void testCheckerWorkflowDownloadBasedOnCredentials() throws IOException {
+        String toolpath = SourceControl.GITHUB.toString() + "/DockstoreTestUser2/md5sum-checker/test";
+        final CommonTestUtilities.TestingPostgres testingPostgres = getTestingPostgres();
+        testingPostgres.runUpdateStatement("update enduser set isadmin = 't' where username = 'DockstoreTestUser2';");
+        final ApiClient webClient = getWebClient();
+        WorkflowsApi workflowApi = new WorkflowsApi(webClient);
+        Workflow workflow = workflowApi
+                .manualRegister(SourceControl.GITHUB.getFriendlyName(), "DockstoreTestUser2/md5sum-checker", "/md5sum/md5sum-workflow.cwl",
+                        "test", "cwl", null);
+        Workflow refresh = workflowApi.refresh(workflow.getId());
+        Assert.assertTrue("workflow is already published for some reason", !refresh.isIsPublished());
+        workflowApi.registerCheckerWorkflow("checker-workflow-wrapping-workflow.cwl", workflow.getId(), "cwl", "checker-input-cwl.json");
+        workflowApi.refresh(workflow.getId());
+
+        final String FILE_WITH_INCORRECT_CREDENTIALS = ResourceHelpers.resourceFilePath("config_file.txt");
+        final String FILE_WITH_CORRECT_CREDENTIALS = ResourceHelpers.resourceFilePath("config_file2.txt");
+
+        // should be able to download properly with correct credentials even though the workflow is not published
+        FileUtils.writeStringToFile(new File("md5sum.input"), "foo" , StandardCharsets.UTF_8);
+        Client.main(new String[] { "--config", FILE_WITH_CORRECT_CREDENTIALS, "checker", "download", "--entry", toolpath, "--version", "master",  "--script" });
+
+        // Publish the workflow
+        Client.main(new String[] { "--config", FILE_WITH_CORRECT_CREDENTIALS, "workflow", "publish", "--entry", toolpath, "--script" });
+
+        // should be able to download properly with incorrect credentials because the entry is published
+        Client.main(new String[] { "--config", FILE_WITH_INCORRECT_CREDENTIALS, "checker", "download", "--entry", toolpath, "--version", "master",  "--script" });
+
+        // Unpublish the workflow
+        Client.main(new String[] { "--config", FILE_WITH_CORRECT_CREDENTIALS, "workflow", "publish", "--entry", toolpath, "--unpub", "--script" });
+
+        // should not be able to download properly with incorrect credentials because the entry is not published
+        systemExit.expectSystemExitWithStatus(Client.ENTRY_NOT_FOUND);
+        Client.main(new String[] { "--config", FILE_WITH_INCORRECT_CREDENTIALS, "checker", "download", "--entry", toolpath, "--version", "master",  "--script" });
+    }
+
+    @Test
+    public void testCheckerWorkflowLaunchBasedOnCredentials() throws IOException {
+        String toolpath = SourceControl.GITHUB.toString() + "/DockstoreTestUser2/md5sum-checker/test";
+        final CommonTestUtilities.TestingPostgres testingPostgres = getTestingPostgres();
+        testingPostgres.runUpdateStatement("update enduser set isadmin = 't' where username = 'DockstoreTestUser2';");
+        final ApiClient webClient = getWebClient();
+        WorkflowsApi workflowApi = new WorkflowsApi(webClient);
+        Workflow workflow = workflowApi
+                .manualRegister(SourceControl.GITHUB.getFriendlyName(), "DockstoreTestUser2/md5sum-checker", "/md5sum/md5sum-workflow.cwl",
+                        "test", "cwl", null);
+        Workflow refresh = workflowApi.refresh(workflow.getId());
+        Assert.assertTrue("workflow is already published for some reason", !refresh.isIsPublished());
+
+        workflowApi.registerCheckerWorkflow("checker-workflow-wrapping-workflow.cwl", workflow.getId(), "cwl", "checker-input-cwl.json");
+
+        refresh = workflowApi.refresh(workflow.getId());
+
+        // should be able to launch properly with correct credentials even though the workflow is not published
+        FileUtils.writeStringToFile(new File("md5sum.input"), "foo" , StandardCharsets.UTF_8);
+        Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "checker", "launch", "--entry", toolpath, "--json" , ResourceHelpers.resourceFilePath("md5sum-wrapper-tool.json"),  "--script" });
+
+        // should be able to launch properly with incorrect credentials but the entry is published
+        Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "workflow", "publish", "--entry", toolpath, "--script" });
+        Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file.txt"), "checker", "launch", "--entry", toolpath, "--json" , ResourceHelpers.resourceFilePath("md5sum-wrapper-tool.json"),  "--script" });
+
+        // should not be able to launch properly with incorrect credentials
+        Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "workflow", "publish", "--entry", toolpath, "--unpub", "--script" });
+        systemExit.expectSystemExitWithStatus(Client.ENTRY_NOT_FOUND);
+        Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file.txt"), "checker", "launch", "--entry", toolpath, "--json" , ResourceHelpers.resourceFilePath("md5sum-wrapper-tool.json"),  "--script" });
     }
 
     @Test
