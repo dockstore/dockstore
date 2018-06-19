31f31bd05ac0b6455d04ef155f171142613ab72a
Denis Yuen <denis.yuen@oicr.on.ca>
2018-06-19 17:01:44 -0400

    Make swagger2open.sh a bit more quiet

--- dockstore-webservice/swagger2open.sh
+++ dockstore-webservice/swagger2open.sh
@@ -4,17 +4,17 @@
 HTTP_STATUS=`curl -F "filename=@src/main/resources/swagger.yaml" https://mermade.org.uk/api/v1/convert -o src/main/resources/open_api.yaml -w "%{http_code}"`
 if [ "$?" -eq 0 ] && [ "$HTTP_STATUS" -eq 200  ]; then
   # Parse out position variable not compatible with open-api schemas
-  sed '/position/d;s/\<html><body><pre>//;w src/main/resources/openapi1.yaml' src/main/resources/open_api.yaml
+  sed --in-place '/position/d;s/<html><body><pre>//' src/main/resources/open_api.yaml
   # Aggregate correct url for dockstore api. Using pipe instead of forward slash, avoid dealing with scape chars
-  sed 's|url: /|url: https://dockstore.org:8443|; w src/main/resources/openapi2.yaml' src/main/resources/openapi1.yaml
+  sed --in-place 's|url: /|url: https://dockstore.org:8443|' src/main/resources/open_api.yaml
 
   # Additional tag for smart-api compliance.
-  sed '/- name: GA4GHV1/i \
-  \ \ - name: NIHdatacommons
-  ' src/main/resources/openapi2.yaml > src/main/resources/openapi.yaml
+  sed --in-place '/- name: GA4GHV1/i \
+  - name: NIHdatacommons
+  ' src/main/resources/open_api.yaml
 
   # Clean directory from excess files.
-  rm src/main/resources/open_api.yaml src/main/resources/openapi1.yaml src/main/resources/openapi2.yaml
+  mv src/main/resources/open_api.yaml src/main/resources/openapi.yaml
 else
   echo "Error converting swagger to openapi, skipping openapi generation"
 fi
