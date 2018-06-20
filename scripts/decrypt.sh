362ffe07b19e1f540ae00fd0f6faa2d1f46e13a5
Denis Yuen <denis.yuen@oicr.on.ca>
2018-06-20 15:03:53 -0400

    Match template from #1527

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -7,7 +7,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-if [ "${TESTING_PROFILE}" = "integration-tests" ] || [ "${TESTING_PROFILE}" = "toil-integration-tests" ]; then
+if [[ "${TESTING_PROFILE}" == *"integration-tests"* ]]; then
     openssl aes-256-cbc -K $encrypted_68a061aa6fa5_key -iv $encrypted_68a061aa6fa5_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
 fi
