0ba898c35abe67a42a975b3a3e54fbb8a3a1d52a
Denis Yuen <denis.yuen@oicr.on.ca>
2018-06-21 10:56:55 -0400

    Fix unit tests

--- dockstore-webservice/src/test/java/io/swagger/api/impl/ToolsImplCommonTest.java
+++ dockstore-webservice/src/test/java/io/swagger/api/impl/ToolsImplCommonTest.java
@@ -153,6 +153,7 @@ public class ToolsImplCommonTest {
         expectedTool.setDescription("");
         expectedTool.setAuthor("sampleAuthor");
         expectedTool.setMetaVersion(null);
+        expectedTool.setAliases(Collections.EMPTY_LIST);
         expectedTool.setContains(Collections.EMPTY_LIST);
         expectedTool.setVerified(false);
         expectedTool.setSigned(false);
@@ -325,6 +326,7 @@ public class ToolsImplCommonTest {
         // Meta-version dates are currently dependant on the environment, disabling for now
         expectedTool.setMetaVersion(null);
         expectedTool.setContains(Collections.EMPTY_LIST);
+        expectedTool.setAliases(Collections.EMPTY_LIST);
         expectedTool.setVerified(true);
         expectedTool.setVerifiedSource("[\"chickenTesterSource\",\"potatoTesterSource\"]");
         expectedTool.setSigned(false);
