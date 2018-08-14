7f6f21ba8524c1a523fc0613cf948e18cefe47ef
Denis Yuen <denis.yuen@oicr.on.ca>
2018-08-14 16:30:35 -0400

    Login message

--- dockstore-webservice/src/main/java/io/dockstore/webservice/resources/TokenResource.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/resources/TokenResource.java
@@ -338,7 +338,7 @@ public class TokenResource implements AuthenticatedResourceInterface, SourceCont
             } else if (user != null) {
                 userID = user.getId();
             } else {
-                throw new CustomWebApplicationException("Something odd happened with Google login ", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+                throw new CustomWebApplicationException("Login failed, you may need to register an account", HttpStatus.SC_UNAUTHORIZED);
             }
 
             List<Token> tokens = tokenDAO.findDockstoreByUserId(userID);
@@ -462,7 +462,7 @@ public class TokenResource implements AuthenticatedResourceInterface, SourceCont
             } else if (user != null) {
                 userID = user.getId();
             } else {
-                throw new CustomWebApplicationException("Something odd happened with GitHub login ", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+                throw new CustomWebApplicationException("Login failed, you may need to register an account", HttpStatus.SC_UNAUTHORIZED);
             }
             List<Token> tokens = tokenDAO.findDockstoreByUserId(userID);
             if (!tokens.isEmpty()) {
