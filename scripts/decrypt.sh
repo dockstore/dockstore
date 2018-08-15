839a8c50a6d700d114c47c81a5cb7de49bf2e049
Denis Yuen <denis.yuen@oicr.on.ca>
2018-08-15 14:41:22 -0400

    update secret archive

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -9,6 +9,6 @@ set -o nounset
 set -o xtrace
 
 if [[ "${TESTING_PROFILE}" == *"integration-tests"* ]]; then
-    openssl aes-256-cbc -K $encrypted_faed3c57a6e9_key -iv $encrypted_faed3c57a6e9_iv -in secrets.tar.enc -out secrets.tar -d
+    openssl aes-256-cbc -K $encrypted_b1f1b0ac05f7_key -iv $encrypted_b1f1b0ac05f7_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
 fi
