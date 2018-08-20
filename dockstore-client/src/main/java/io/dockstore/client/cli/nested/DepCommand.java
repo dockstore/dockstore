e4cf310b057200fdb8104875e7e3b138d4d3b1fd
Gary Luu <garylau.work@gmail.com>
2018-08-20 16:16:46 -0400

    Handle unknown runner (#1727)
    * Handle unknown runner
    
    * Show options
    
    * Revert this once there's another runner deps to list

--- dockstore-client/src/main/java/io/dockstore/client/cli/nested/DepCommand.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/nested/DepCommand.java
@@ -9,6 +9,8 @@ import io.swagger.client.ApiClient;
 import io.swagger.client.Configuration;
 import io.swagger.client.api.MetadataApi;
 
+import static io.dockstore.client.cli.Client.API_ERROR;
+
 /**
  * @author gluu
  * @since 12/04/18
@@ -35,18 +37,23 @@ public final class DepCommand {
             MetadataApi metadataApi = new MetadataApi(defaultApiClient);
             String runnerDependencies = metadataApi
                     .getRunnerDependencies(commandDep.clientVersion, commandDep.pythonVersion, commandDep.runner, "text");
-            ArgumentUtility.out(runnerDependencies);
+            if (runnerDependencies == null) {
+                ArgumentUtility.errorMessage("Could not get runner dependencies", API_ERROR);
+            } else {
+                ArgumentUtility.out(runnerDependencies);
+            }
+
         }
         return true;
     }
 
-    @Parameters(separators = "=", commandDescription = "Print tool/workflow runner dependencies")
+    @Parameters(separators = "=", commandDescription = "Print cwltool runner dependencies")
     private static class CommandDep {
         @Parameter(names = "--client-version", description = "Dockstore version")
         private String clientVersion = getClass().getPackage().getImplementationVersion();
         @Parameter(names = "--python-version", description = "Python version")
         private String pythonVersion = "2";
-        @Parameter(names = "--runner", description = "tool/workflow runner")
+        // @Parameter(names = "--runner", description = "tool/workflow runner. Available options: 'cwltool'")
         private String runner = "cwltool";
         @Parameter(names = "--help", description = "Prints help for deps", help = true)
         private boolean help = false;
