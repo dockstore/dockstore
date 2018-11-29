e9bae59dbbd7ea6ea95b92b8be2c27bbfe53d7f6
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-11-29 12:57:15 -0500

    remove unused imports

--- dockstore-client/src/main/java/io/dockstore/client/cli/nested/CromwellLauncher.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/nested/CromwellLauncher.java
@@ -1,8 +1,6 @@
 package io.dockstore.client.cli.nested;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
@@ -10,8 +8,6 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import com.google.common.io.Files;
 import com.google.gson.Gson;
