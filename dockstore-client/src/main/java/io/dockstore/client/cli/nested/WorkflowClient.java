4d755f6c3b349ad853e5b435717339aaf445b5e3
Louise Cabansay <ldcabansay@gmail.com>
2018-12-27 13:22:01 -0800

    move pre-validate method to before parsing

--- dockstore-client/src/main/java/io/dockstore/client/cli/nested/WorkflowClient.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/nested/WorkflowClient.java
@@ -390,6 +390,7 @@ public class WorkflowClient extends AbstractEntryClient<Workflow> {
     @Override
     public void launch(final List<String> args) {
         String commandName = "launch";
+        preValidateLaunchArguments(args);
         String[] argv = args.toArray(new String[0]);
         String[] argv1 = { commandName };
         String[] both = ArrayUtils.addAll(argv1, argv);
@@ -401,7 +402,7 @@ public class WorkflowClient extends AbstractEntryClient<Workflow> {
         String tsvRun = commandLaunch.tsv;
         String wdlOutputTarget = commandLaunch.wdlOutputTarget;
         String uuid = commandLaunch.uuid;
-        preValidateLaunchArguments(args);
+
 
         // trim the final slash on output if it is present, probably an error ( https://github.com/aws/aws-cli/issues/421 ) causes a double slash which can fail
         wdlOutputTarget = wdlOutputTarget != null ? wdlOutputTarget.replaceAll("/$", "") : null;
