16908b867f2b419e68f8c0333a72e8d8bba7ddd0
Denis Yuen <denis.yuen@oicr.on.ca>
2019-02-08 18:24:03 -0500

    Try to stabilize openapi generation

---
+++ dockstore-webservice/src/main/java/io/swagger/OrderedObjectMapperProcessor.java
@@ -0,0 +1,32 @@
+/*
+ *    Copyright 2019 OICR
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package io.swagger;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.integration.api.ObjectMapperProcessor;
+
+public class OrderedObjectMapperProcessor implements ObjectMapperProcessor {
+    @Override
+    public void processJsonObjectMapper(ObjectMapper mapper) {
+        /** do nothing */
+    }
+
+    @Override
+    public void processYamlObjectMapper(ObjectMapper mapper) {
+        mapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+    }
+}
