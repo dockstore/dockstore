70842ba9d41c6ffc7f460de757d9f3715cbb603b
Gary Luu <garylau.work@gmail.com>
2019-03-26 10:43:30 -0400

    Change default server url (#2277)

--- dockstore-client/src/main/java/io/dockstore/client/cli/Client.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/Client.java
@@ -774,7 +774,7 @@ public class Client {
         INIConfiguration config = getIniConfiguration(args);
         // pull out the variables from the config
         String token = config.getString("token", "");
-        String serverUrl = config.getString("server-url", "https://www.dockstore.org/api");
+        String serverUrl = config.getString("server-url", "https://dockstore.org/api");
         ApiClient defaultApiClient;
         defaultApiClient = Configuration.getDefaultApiClient();
         String cliVersion = getClientVersion();
