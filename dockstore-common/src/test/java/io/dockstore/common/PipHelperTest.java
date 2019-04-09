a86cd70e216a09e584abfe54d9a466c7f6159615
Denis Yuen <denis.yuen@oicr.on.ca>
2019-04-09 16:04:01 -0400

    Fix response for 1.5.4

--- dockstore-common/src/test/java/io/dockstore/common/PipHelperTest.java
+++ dockstore-common/src/test/java/io/dockstore/common/PipHelperTest.java
@@ -3,24 +3,26 @@ package io.dockstore.common;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * @author gluu
  * @since 14/08/18
  */
 public class PipHelperTest {
     @Test
-    public void convertSemVerToAvailableVersion() throws Exception {
+    public void convertSemVerToAvailableVersion() {
+        Assert.assertEquals("1.7.0", PipHelper.convertSemVerToAvailableVersion("1.7.0-snapshot"));
+        Assert.assertEquals("1.6.0", PipHelper.convertSemVerToAvailableVersion("1.6.4"));
+        // TODO: expect 1.7.0 on develop
         Assert.assertEquals("1.6.0", PipHelper.convertSemVerToAvailableVersion(PipHelper.DEV_SEM_VER));
         Assert.assertEquals("1.6.0", PipHelper.convertSemVerToAvailableVersion(null));
         Assert.assertEquals("1.6.0", PipHelper.convertSemVerToAvailableVersion("1.6.0-snapshot"));
         Assert.assertEquals("1.6.0", PipHelper.convertSemVerToAvailableVersion("1.6.0"));
+        Assert.assertEquals("1.5.0", PipHelper.convertSemVerToAvailableVersion("1.5.4"));
         Assert.assertEquals("1.5.0", PipHelper.convertSemVerToAvailableVersion("1.5.0-snapshot"));
         Assert.assertEquals("1.5.0", PipHelper.convertSemVerToAvailableVersion("1.5.0"));
         Assert.assertEquals("1.4.0", PipHelper.convertSemVerToAvailableVersion("1.4.0-snapshot"));
         Assert.assertEquals("1.4.0", PipHelper.convertSemVerToAvailableVersion("1.4.0"));
-        Assert.assertEquals("1.4.0", PipHelper.convertSemVerToAvailableVersion("1.3.0-snapshot"));
-        Assert.assertEquals("1.4.0", PipHelper.convertSemVerToAvailableVersion("1.3.0"));
+        Assert.assertEquals("1.3.0", PipHelper.convertSemVerToAvailableVersion("1.3.0-snapshot"));
+        Assert.assertEquals("1.3.0", PipHelper.convertSemVerToAvailableVersion("1.3.0"));
     }
 }
