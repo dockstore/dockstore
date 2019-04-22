2693f889a9750838364641e2e7ebf4dfd6c41740
natalieperez <nperez9@ucsc.eduo>
2019-04-22 10:41:49 -0700

    Keep swagger and openapi3 consistent.

--- dockstore-webservice/src/main/java/io/dockstore/webservice/resources/MetadataResource.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/resources/MetadataResource.java
@@ -117,8 +117,8 @@ public class MetadataResource {
     @Timed
     @UnitOfWork(readOnly = true)
     @Path("sitemap")
-    @Operation(summary = "List all available workflow, tool, organization, and collection paths", description = "List all available workflow, tool, organization, and collection paths. Available means published for tools/workflows, and approved for organizations and their respective collections. NO authentication")
-    @ApiOperation(value = "List all available workflow, tool, organization, and collection paths.", notes = "NO authentication")
+    @Operation(summary = "List all available workflow, tool, organization, and collection paths. Available means published for tools/workflows, and approved for organizations and their respective collections.", description = "List all available workflow, tool, organization, and collection paths. Available means published for tools/workflows, and approved for organizations and their respective collections. NO authentication")
+    @ApiOperation(value = "List all available workflow, tool, organization, and collection paths. Available means published for tools/workflows, and approved for organizations and their respective collections.", notes = "NO authentication")
     public String sitemap() {
         //TODO needs to be more efficient via JPA query
         List<Tool> tools = toolDAO.findAllPublished();
