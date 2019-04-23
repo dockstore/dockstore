26870aebf7a5ad92f77bd36ea2e05d9f156aaf48
Charles Overbeck <charles.overbeck@gmail.com>
2019-04-23 16:43:43 -0700

    Feature/2385/config (#2401)
    Expose config information via API for UI
    
    #2385
    
    Adds a new endpoint, /metadata/config.json to retrieve information to
    be used by the UI.
    
    As part of this, make githubClientID and githubClientSecret
    simple strings -- they were List<String>. The List was to
    support the transition from ui1 to ui2, but now that the UI will soon
    be retrieving the client id from the web service, it no longer
    makes sense.

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -9,6 +9,6 @@ set -o nounset
 set -o xtrace
 
 if [[ "${TESTING_PROFILE}" == *"integration-tests"* ]] || [[ "${TESTING_PROFILE}" == "automated-review" ]]; then
-    openssl aes-256-cbc -K $encrypted_5454045417e3_key -iv $encrypted_5454045417e3_iv -in secrets.tar.enc -out secrets.tar -d
+    openssl aes-256-cbc -K $encrypted_b3942f249f21_key -iv $encrypted_b3942f249f21_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
 fi
