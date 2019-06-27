ed4d5597241cf5114e2ebb27b82edc917e14e2cc
Emily Soth <esoth@ucsc.edu>
2019-06-27 13:06:16 -0700

    removed unused import

--- dockstore-client/src/main/java/io/dockstore/client/cli/nested/AbstractEntryClient.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/nested/AbstractEntryClient.java
@@ -31,7 +31,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
