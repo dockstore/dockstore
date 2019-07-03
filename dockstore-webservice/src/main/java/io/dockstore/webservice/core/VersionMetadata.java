16a21c5b5ffbf4e0ab36d691e5626cf264193475
Denis Yuen <denis.yuen@oicr.on.ca>
2019-07-03 17:08:44 -0400

    Forgot file

---
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/VersionMetadata.java
@@ -0,0 +1,57 @@
+/*
+ *    Copyright 2019 OICR
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package io.dockstore.webservice.core;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Data about versions of a workflow/tool in Dockstore rather than about the original workflow. Stays non-immutable
+ */
+@Entity
+@Table(name = "version_metadata")
+public class VersionMetadata {
+    @Column(columnDefinition =  "boolean default false")
+    protected boolean verified;
+
+    @Column()
+    protected String verifiedSource;
+
+    @Column()
+    protected String doiURL;
+
+    @Column()
+    protected boolean hidden;
+
+    @Column(columnDefinition = "text default 'NOT_REQUESTED'", nullable = false)
+    @Enumerated(EnumType.STRING)
+    protected Version.DOIStatus doiStatus;
+
+    @Id
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
