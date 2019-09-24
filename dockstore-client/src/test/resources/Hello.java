b7f63cdef160228a141c21196976300251646b98
Denis Yuen <denis.yuen@oicr.on.ca>
2019-09-24 15:18:56 -0400

    Remove dockstore-client and local launch tests (#2892)
    * moving toil tests to dockstore-cli if anything
    * simplifying profiles and associated tests in travis
    * moving out tests that are mostly about running workflows and tools

--- dockstore-client/src/test/resources/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
