5308a0e48d9357f9c0950d3557a60446c98eddb1
Charles Overbeck <coverbec@ucsc.edu>
2019-10-11 09:08:34 -0700

    PR feedback

--- dockstore-common/src/main/java/io/dockstore/common/MemoryIntensiveTest.java
+++ dockstore-common/src/main/java/io/dockstore/common/MemoryIntensiveTest.java
@@ -1,4 +1,10 @@
 package io.dockstore.common;
 
+/**
+ * For tests that use a lot of memory. On Circle CI, unit tests were frequently failing
+ * due to a Java VM 137 exit code, which is the VM running out of memory.
+ * Tests using this annotation will be run via a separate Maven profile, in a new VM,
+ * enabling both sets of test to finish without running out of memory.
+ */
 public interface MemoryIntensiveTest {
 }
