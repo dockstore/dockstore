636b01c23165153f3379cc717be2ae369c4869b3
NatalieEO <nperez9@ucsc.edu>
2019-10-25 17:13:37 -0700

    forgot this file

---
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/Checksum.java
@@ -0,0 +1,49 @@
+/*
+ *    Copyright 2019 OICR
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package io.dockstore.webservice.core;
+
+public class Checksum {
+
+    private String type;
+
+    private String checksum;
+
+    public Checksum() {
+
+    }
+
+    public Checksum(String type, String checksum) {
+        this.type = type;
+        this.checksum = checksum;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(String checksum) {
+        this.checksum = checksum;
+    }
+}
