20958aedc0201bb1c76e45c4c9eda5d4f9361486
NatalieEO <nperez9@ucsc.edu>
2019-12-03 20:53:14 -0800

    gitlab checksums

---
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/gitlab/GitLabContainerRegistry.java
@@ -0,0 +1,58 @@
+package io.dockstore.webservice.core.gitlab;
+
+public class GitLabContainerRegistry {
+    private long id;
+    private String name;
+    private String path;
+    private long projectID;
+    private String location;
+    private String createdAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public long getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(long projectID) {
+        this.projectID = projectID;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
