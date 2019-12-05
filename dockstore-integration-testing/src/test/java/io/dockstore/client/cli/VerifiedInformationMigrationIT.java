99fced23a6ee09c7cee3ac2a34b7133947011933
Gary Luu <garylau.work@gmail.com>
2019-12-05 10:16:55 -0500

    Remove deprecated test (#3048)
    * Remove deprecated test
    
    * fix imports

--- dockstore-integration-testing/src/test/java/io/dockstore/client/cli/VerifiedInformationMigrationIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/client/cli/VerifiedInformationMigrationIT.java
@@ -4,13 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import io.dockstore.common.CommonTestUtilities;
-import io.dockstore.common.SourceControl;
 import io.dockstore.common.TestingPostgres;
 import io.dockstore.webservice.DockstoreWebserviceApplication;
 import io.dockstore.webservice.DockstoreWebserviceConfiguration;
 import io.dropwizard.Application;
 import io.dropwizard.testing.DropwizardTestSupport;
-import io.dropwizard.testing.ResourceHelpers;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -74,37 +72,6 @@ public class VerifiedInformationMigrationIT {
         final long afterMigrationVerifiedCount = testingPostgres.runSelectStatement("select count(*) from sourcefile_verified", long.class);
         Assert
             .assertEquals("There should be 2 entries in sourcefile_verified after the migration but got: " + afterMigrationVerifiedCount, 5,
-                afterMigrationVerifiedCount);
-    }
-
-    @Test
-    public void workflowVerifiedInformationMigrationTest() {
-        Application<DockstoreWebserviceConfiguration> application = SUPPORT.getApplication();
-        try {
-            application.run("db", "drop-all", "--confirm-delete-everything", CommonTestUtilities.CONFIDENTIAL_CONFIG_PATH);
-            List<String> migrationList = Arrays.asList("1.3.0.generated", "1.3.1.consistency", "1.4.0", "testworkflow");
-            CommonTestUtilities.runMigration(migrationList, application, CommonTestUtilities.CONFIDENTIAL_CONFIG_PATH);
-        } catch (Exception e) {
-            Assert.fail("Could not run migrations up to 1.4.0");
-        }
-
-        testingPostgres.runUpdateStatement("UPDATE workflowversion SET verified='t' where name='master'");
-
-        Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "workflow", "verify", "--entry",
-            SourceControl.GITHUB.toString() + "/testWorkflow/testWorkflow", "--verified-source", "Docker testing group", "--version",
-            "master", "--script" });
-
-        // Run full 1.5.0 migration
-        try {
-            List<String> migrationList = Arrays.asList("1.5.0");
-            CommonTestUtilities.runMigration(migrationList, application, CommonTestUtilities.CONFIDENTIAL_CONFIG_PATH);
-        } catch (Exception e) {
-            Assert.fail("Could not run 1.5.0 migration");
-        }
-
-        final long afterMigrationVerifiedCount = testingPostgres.runSelectStatement("select count(*) from sourcefile_verified", long.class);
-        Assert
-            .assertEquals("There should be 2 entries in sourcefile_verified after the migration but got: " + afterMigrationVerifiedCount, 2,
                 afterMigrationVerifiedCount);
     }
 }
