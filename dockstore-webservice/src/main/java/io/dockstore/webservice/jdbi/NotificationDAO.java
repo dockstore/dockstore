df9cd81579affdc1631b27bdef0006a97bcc5011
emlys <43770515+emlys@users.noreply.github.com>
2020-01-01 19:36:37 -0800

    Feature/seab 563/notifications backend (#2986)
    * four endpoints working
    
    * added integration tests
    
    * update null check
    
    * tests passing
    
    * removed unused import
    
    * added get/set for db dates
    
    * 401 status code
    
    * re-base and update ymls
    
    Co-authored-by: Louise Cabansay <lcabansa@ucsc.edu>

---
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/jdbi/NotificationDAO.java
@@ -0,0 +1,39 @@
+package io.dockstore.webservice.jdbi;
+
+import java.util.List;
+
+import io.dockstore.webservice.core.Notification;
+import io.dropwizard.hibernate.AbstractDAO;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class NotificationDAO extends AbstractDAO<Notification> {
+    public NotificationDAO(SessionFactory factory) {
+        super(factory);
+    }
+
+    public long create(Notification notification) {
+        return persist(notification).getId();
+    }
+
+    public Notification update(Notification notification) {
+        Session session = currentSession();
+        session.update(notification);
+        session.flush();
+        return notification;
+    }
+
+    public void delete(Notification notification) {
+        Session session = currentSession();
+        session.delete(notification);
+        session.flush();
+    }
+
+    public Notification findById(long id) {
+        return get(id);
+    }
+
+    public List<Notification> getActiveNotifications() {
+        return list(namedQuery("io.dockstore.webservice.core.Notification.getActiveNotifications"));
+    }
+}
---
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/resources/NotificationResource.java
@@ -0,0 +1,112 @@
+package io.dockstore.webservice.resources;
+
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.dockstore.webservice.CustomWebApplicationException;
+import io.dockstore.webservice.core.Notification;
+import io.dockstore.webservice.jdbi.NotificationDAO;
+import io.dropwizard.hibernate.UnitOfWork;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
+import org.apache.http.HttpStatus;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.dockstore.webservice.Constants.JWT_SECURITY_DEFINITION_NAME;
+
+@Path("/curation")
+@Api("/curation")
+@Produces(MediaType.APPLICATION_JSON)
+public class NotificationResource {
+    private static final Logger LOG = LoggerFactory.getLogger(NotificationResource.class);
+
+    // interface between the endpoint and the database
+    private final NotificationDAO notificationDAO;
+
+    // constructor
+    public NotificationResource(SessionFactory sessionFactory) {
+        this.notificationDAO = new NotificationDAO(sessionFactory);
+    }
+
+    // get a notification by its id
+    @GET
+    @Path("/notifications/{id}")
+    @UnitOfWork
+    @ApiOperation(value = "Return the notification with given id", notes = "NO Authentication", responseContainer = "List", response = Notification.class)
+    public Notification getNotification(@PathParam("id") Long id) {
+        Notification notification = notificationDAO.findById(id);
+        throwErrorIfNull(notification);
+        return notification;
+    }
+
+    // get all active notifications
+    @GET
+    @Path("/notifications")
+    @UnitOfWork
+    @ApiOperation(value = "Return all active notifications", notes = "NO Authentication", responseContainer = "List", response = Notification.class)
+    public List<Notification> getActiveNotifications() {
+        return notificationDAO.getActiveNotifications();
+    }
+
+    // post a new notification
+    @POST
+    @Path("/notifications")
+    @UnitOfWork
+    @RolesAllowed({"curator", "admin"})
+    @ApiOperation(value = "Create a notification", authorizations = {@Authorization(value = JWT_SECURITY_DEFINITION_NAME)},
+            notes = "Curator/admin only", response = Notification.class)
+    public Notification createNotification(@ApiParam(value = "Notification to create", required = true) Notification notification) {
+        long id = notificationDAO.create(notification);
+        return notificationDAO.findById(id);
+    }
+
+    // delete a notification by its id
+    @DELETE
+    @Path("/notifications/{id}")
+    @UnitOfWork
+    @RolesAllowed({ "curator", "admin" })
+    @ApiOperation(value = "Delete a notification", authorizations = {@Authorization(value = JWT_SECURITY_DEFINITION_NAME)}, notes = "Curator/admin only")
+    public void deleteNotification(@ApiParam(value = "Notification to delete", required = true) @PathParam("id") Long id) {
+        Notification notification = notificationDAO.findById(id);
+        throwErrorIfNull(notification);
+        notificationDAO.delete(notification);
+    }
+
+    @PUT
+    @Path("/notifications/{id}")
+    @UnitOfWork
+    @RolesAllowed({ "curator", "admin" })
+    @ApiOperation(value = "Update a notification", authorizations = {@Authorization(value = JWT_SECURITY_DEFINITION_NAME)},
+            notes = "Curator/admin only", response = Notification.class)
+    public Notification updateNotification(@ApiParam(value = "Notification to update", required = true) @PathParam("id") long id,
+                                           @ApiParam(value = "Updated version of notification", required = true) Notification notification) {
+        if (id != notification.getId()) {
+            String msg = "ID in path and notification param must match";
+            LOG.info(msg);
+            throw new CustomWebApplicationException(msg, HttpStatus.SC_BAD_REQUEST);
+        }
+        return notificationDAO.update(notification);
+    }
+
+    private void throwErrorIfNull(Notification notification) {
+        if (notification == null) {
+            String msg = "Notification not found";
+            LOG.info(msg);
+            throw new CustomWebApplicationException(msg, HttpStatus.SC_NOT_FOUND);
+        }
+    }
+
+}
