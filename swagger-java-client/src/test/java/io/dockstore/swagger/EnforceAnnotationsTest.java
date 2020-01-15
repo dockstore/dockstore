11749da0047599a0b5494a123b8a5d1950721d20
Denis Yuen <denis.yuen@oicr.on.ca>
2020-01-15 14:27:09 -0500

    Make codacy happier

--- swagger-java-client/src/test/java/io/dockstore/swagger/EnforceAnnotationsTest.java
+++ swagger-java-client/src/test/java/io/dockstore/swagger/EnforceAnnotationsTest.java
@@ -8,19 +8,19 @@ import org.junit.Test;
 
 public class EnforceAnnotationsTest {
 
-
     @Test
     public void checkAnnotations() {
-        // we had a bizarre issue where the Workflow would lose an annotation
+        // we had a bizarre issue where the Workflow would lose an annotation that was required for dockstore-cli to work properly
         Workflow workflow = new Workflow();
         Class<? extends Workflow> cls = workflow.getClass();
         final Annotation[] annotations = cls.getAnnotations();
         boolean hasJsonSubTypes = false;
         for (Annotation an : annotations) {
-            if (an.toString().startsWith("@com.fasterxml.jackson.annotation.JsonSubTypes(")) {
-                if (an.toString().contains("name=\"Service\", value=io.swagger.client.model.Service.class") && an.toString().contains("name=\"BioWorkflow\", value=io.swagger.client.model.BioWorkflow.class")) {
-                    hasJsonSubTypes = true;
-                }
+            final String toString = an.toString();
+            if (toString.startsWith("@com.fasterxml.jackson.annotation.JsonSubTypes(") && toString
+                .contains("name=\"Service\", value=io.swagger.client.model.Service.class") && toString
+                .contains("name=\"BioWorkflow\", value=io.swagger.client.model.BioWorkflow.class")) {
+                hasJsonSubTypes = true;
             }
         }
         Assert.assertTrue(hasJsonSubTypes);
