f8a6581e92c709d581465a2fa6f1b7e817b721b9
Charles Overbeck <coverbec@ucsc.edu>
2020-02-10 14:08:44 -0800

    Add configuration for BioData Catalyst buttons

--- dockstore-webservice/src/main/java/io/dockstore/webservice/DockstoreWebserviceConfiguration.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/DockstoreWebserviceConfiguration.java
@@ -614,6 +614,8 @@ public class DockstoreWebserviceConfiguration extends Configuration {
         private String dnaStackImportUrl;
         private String dnaNexusImportUrl;
         private String terraImportUrl;
+        private String biodataCatalystWdlUrl;
+        private String biodataCatalystCwlUrl;
 
         private String gitHubAuthUrl;
         private String gitHubRedirectPath;
@@ -667,6 +669,22 @@ public class DockstoreWebserviceConfiguration extends Configuration {
 
         public void setTerraImportUrl(String terraImportUrl) {
             this.terraImportUrl = terraImportUrl;
+        }
+
+        public String getBiodataCatalystWdlUrl() {
+            return biodataCatalystWdlUrl;
+        }
+
+        public void setBiodataCatalystWdlUrl(String biodataCatalystWdlUrl) {
+            this.biodataCatalystWdlUrl = biodataCatalystWdlUrl;
+        }
+
+        public String getBiodataCatalystCwlUrl() {
+            return biodataCatalystCwlUrl;
+        }
+
+        public void setBiodataCatalystCwlUrl(String biodataCatalystCwlUrl) {
+            this.biodataCatalystCwlUrl = biodataCatalystCwlUrl;
         }
 
         public String getGitHubAuthUrl() {
