56237790d38cc1726a16d83fc395e58ea78b2bb8
Louise Cabansay <ldcabansay@gmail.com>
2020-02-14 12:11:39 -0800

    add git-commit-id-maven-plugin, read generated git.properties file, and expose webservice git commit info (ID and build version) using metadata/config endpoint

---
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/ConfigHelper.java
@@ -0,0 +1,46 @@
+package io.dockstore.webservice.helpers;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Helper class for setting the config variables for git commit info
+ */
+public final class ConfigHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigHelper.class);
+
+    private ConfigHelper() {
+    }
+
+    /**
+     * Reads a properties file with git info and returns abbreviated commitId and build version.
+     * If file cannot be read, will log error and set config to "git property not found".
+     * @param fileName properties file with git information
+     */
+    public static GitInfo readGitProperties(String fileName) {
+        final Properties properties = new Properties();
+        LOGGER.info("Retrieving git properties");
+        try {
+            properties.load(GitInfo.class.getClassLoader().getResourceAsStream(fileName));
+            return new GitInfo(String.valueOf(properties.get("git.commit.id.abbrev")), String.valueOf(properties.get("git.build.version")));
+        } catch (Exception e) {
+            LOGGER.error("Could not load git.properties", e);
+            String propertyNotFound = "git property not found";
+            return new GitInfo(propertyNotFound, propertyNotFound);
+        }
+    }
+
+    public static class GitInfo {
+        public final String commitId;
+        public final String buildVersion;
+
+        public GitInfo(String commitId, String buildVersion) {
+            this.commitId = commitId;
+            this.buildVersion = buildVersion;
+        }
+    }
+
+}
