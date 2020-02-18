fab6c21b9ee442c41d4e69cd348672b59a0b80da
Denis Yuen <denis.yuen@oicr.on.ca>
2020-02-18 17:41:58 -0500

    Put back DAG cleaning

--- dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/DAGHelper.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/DAGHelper.java
@@ -19,7 +19,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import io.dockstore.webservice.core.dag.ElementsDefinition;
 
 /**
@@ -40,14 +39,10 @@ public final class DAGHelper {
      * @return The clean DAG without edges that have undefined nodes
      */
     public static String cleanDAG(String dag) {
-        try {
-            ElementsDefinition elementsDefinition = GSON.fromJson(dag, ElementsDefinition.class);
-            Set<String> nodeIDs = elementsDefinition.nodes.stream().map(nodeDefinition -> nodeDefinition.data.id).collect(Collectors.toSet());
-            elementsDefinition.edges = elementsDefinition.edges.stream().filter(edgeDefinition -> nodeIDs.contains(edgeDefinition.data.source)).collect(Collectors.toList());
-            return GSON.toJson(elementsDefinition);
-        } catch (JsonSyntaxException e) {
-            // not awesome, but galaxy dag seems malformed according to GSON
-            return dag;
-        }
+        ElementsDefinition elementsDefinition = GSON.fromJson(dag, ElementsDefinition.class);
+        Set<String> nodeIDs = elementsDefinition.nodes.stream().map(nodeDefinition -> nodeDefinition.data.id).collect(Collectors.toSet());
+        elementsDefinition.edges = elementsDefinition.edges.stream().filter(edgeDefinition -> nodeIDs.contains(edgeDefinition.data.source))
+            .collect(Collectors.toList());
+        return GSON.toJson(elementsDefinition);
     }
 }
