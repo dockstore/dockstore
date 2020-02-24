29e742381b38061aa1601fed23aad74c8813555a
Gary Luu <garylau.work@gmail.com>
2020-02-24 18:17:55 -0500

    Update sortOpenAPIYaml.sh

--- dockstore-webservice/scripts/sortOpenAPIYaml.sh
+++ dockstore-webservice/scripts/sortOpenAPIYaml.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+# https://github.com/mikefarah/yq Require version 3.1.2.  Newer will likely also work.
 yq r --tojson src/main/resources/openapi3/unsortedopenapi.yaml | yq r - -P > src/main/resources/openapi3/openapi.yaml
