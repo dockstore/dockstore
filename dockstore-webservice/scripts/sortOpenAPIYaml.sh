4c966118b53f31e5e5b353ce111c7aa9b2fe61ef
garyluu <garylau.work@gmail.com>
2020-02-25 10:59:06 -0500

    Retrigger LGTM

--- dockstore-webservice/scripts/sortOpenAPIYaml.sh
+++ dockstore-webservice/scripts/sortOpenAPIYaml.sh
@@ -2,5 +2,5 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-# https://github.com/mikefarah/yq Require version 3.1.2.  Newer will likely also work.
+# https://github.com/mikefarah/yq Require version 3.1.2.  Newer version will likely also work.
 yq r --tojson src/main/resources/openapi3/unsortedopenapi.yaml | yq r - -P > src/main/resources/openapi3/openapi.yaml
