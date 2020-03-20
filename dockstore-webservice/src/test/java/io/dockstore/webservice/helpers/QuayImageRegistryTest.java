c754ed2a55c09cad1410e6ffc69723f3b06dc2d7
garyluu <garylau.work@gmail.com>
2020-03-20 11:06:59 -0400

    Add test

---
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/QuayImageRegistryTest.java
@@ -0,0 +1,41 @@
+package io.dockstore.webservice.helpers;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import io.dockstore.webservice.core.Tag;
+import io.dockstore.webservice.core.Token;
+import io.dockstore.webservice.core.Tool;
+import io.dockstore.webservice.core.Version;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuayImageRegistryTest {
+
+    /**
+     * Normally, we are only able to get 500 tags without using the paginated endpoint.
+     * This tests that when there are over 500 tags, we can use the paginated endpoint to retrieve all of them instead.
+     * Using calico/node because it has over 3838 tags
+     */
+    @Test
+    public void getOver500TagsTest() {
+        Token token = new Token();
+        token.setContent("fakeQuayTokenBecauseWeDontReallyNeedOne");
+        QuayImageRegistry quayImageRegistry = new QuayImageRegistry(token);
+        Tool tool = new Tool();
+        tool.setRegistry("quay.io");
+        tool.setNamespace("calico");
+        tool.setName("node");
+        List<Tag> tags = quayImageRegistry.getTags(tool);
+        int size = tags.size();
+        Assert.assertTrue("Should be able to get more than the default 500 tags", size > 3838);
+        tags.forEach(tag -> {
+            Assert.assertNotEquals("Image ID should be populated", null, tag.getImageId());
+            Assert.assertTrue("Images should be populated", tag.getImages().size() > 0);
+        });
+        Set<String> collect = tags.parallelStream().map(Version::getName).collect(Collectors.toSet());
+        int distinctSize = collect.size();
+        Assert.assertEquals("There should be no tags with the same name", size, distinctSize);
+    }
+}
