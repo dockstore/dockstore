920bce247092f61deb679a5a50e82e160dcdc7a5
Andrew Duncan <andrew.duncan@oicr.on.ca>
2020-05-06 15:14:35 -0400

    remove redundant LOG

--- dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/GitHubSourceCodeRepo.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/GitHubSourceCodeRepo.java
@@ -397,7 +397,6 @@ public class GitHubSourceCodeRepo extends SourceCodeRepoInterface {
                 WorkflowVersion existingVersion = existingDefaults.get(ref.getLeft());
                 // Only add if version doesn't already exist, or it exists but commit ID is null or does not match
                 if (hardRefresh || existingVersion == null || existingVersion.getCommitID() == null || !Objects.equals(existingVersion.getCommitID(), ref.getRight())) {
-                    LOG.info("Refreshing " + ref.getLeft() + " " + ref.getRight());
                     WorkflowVersion version = setupWorkflowVersionsHelper(workflow, ref, existingWorkflow, existingDefaults,
                             repository, null, versionName);
                     if (version != null) {
