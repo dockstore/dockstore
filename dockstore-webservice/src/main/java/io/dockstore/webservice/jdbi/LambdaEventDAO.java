df236e9d0cb1ec5766b63683379e51de5639b9c3
Andrew Duncan <andrew.duncan@oicr.on.ca>
2020-05-14 15:42:11 -0400

    forgot to add new dao file

---
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/jdbi/LambdaEventDAO.java
@@ -0,0 +1,52 @@
+package io.dockstore.webservice.jdbi;
+
+import java.util.List;
+
+import io.dockstore.webservice.core.LambdaEvent;
+import io.dockstore.webservice.core.User;
+import io.dropwizard.hibernate.AbstractDAO;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+public class LambdaEventDAO extends AbstractDAO<LambdaEvent> {
+    public LambdaEventDAO(SessionFactory factory) {
+        super(factory);
+    }
+
+    public LambdaEvent findById(Long id) {
+        return get(id);
+    }
+
+    public long create(LambdaEvent lambdaEvent) {
+        return persist(lambdaEvent).getId();
+    }
+
+    public long update(LambdaEvent lambdaEvent) {
+        return persist(lambdaEvent).getId();
+    }
+
+    public void delete(LambdaEvent lambdaEvent) {
+        Session session = currentSession();
+        session.delete(lambdaEvent);
+        session.flush();
+    }
+
+    public List<LambdaEvent> findByRepository(String repository) {
+        Query query = namedQuery("io.dockstore.webservice.core.LambdaEvent.findByRepository")
+                .setParameter("repository", repository);
+        return list(query);
+    }
+
+    public List<LambdaEvent> findByUsername(String username) {
+        Query query = namedQuery("io.dockstore.webservice.core.LambdaEvent.findByUsername")
+                .setParameter("username", username);
+        return list(query);
+    }
+
+    public List<LambdaEvent> findByUser(User user) {
+        Query query = namedQuery("io.dockstore.webservice.core.LambdaEvent.findByUser")
+                .setParameter("user", user);
+        return list(query);
+    }
+}
