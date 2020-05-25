4fca6da39f395ce38ed3ad77ca111cf6f7d6a7ed
garyluu <garylau.work@gmail.com>
2020-05-25 13:32:45 -0400

    Remove derived fields and setters

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/MyWorkflows.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/MyWorkflows.java
@@ -1,6 +1,5 @@
 package io.dockstore.webservice.core;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dockstore.common.SourceControl;
 
 /**
@@ -38,93 +37,35 @@ public class MyWorkflows {
         return mode;
     }
 
-    public void setMode(WorkflowMode mode) {
-        this.mode = mode;
-    }
-
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @JsonProperty
     public String getGitUrl() {
-        if (mode == WorkflowMode.HOSTED) {
-            // for a dockstore hosted workflow, fake a git url. Used by the UI
-            return "git@dockstore.org:workflows/" + this.getWorkflowPath() + ".git";
-        }
-        return this.getGitUrl2();
+        return this.gitUrl;
     }
 
     public String getOrganization() {
         return organization;
     }
 
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
     public String getWorkflowName() {
         return workflowName;
-    }
-
-    public void setWorkflowName(String workflowName) {
-        this.workflowName = workflowName;
     }
 
     public String getRepository() {
         return repository;
     }
 
-    public void setRepository(String repository) {
-        this.repository = repository;
-    }
-
     public SourceControl getSourceControl() {
         return sourceControl;
     }
 
-    public void setSourceControl(SourceControl sourceControl) {
-        this.sourceControl = sourceControl;
-    }
-
-    @JsonProperty("full_workflow_path")
-    public String getWorkflowPath() {
-        return getPath() + (workflowName == null || "".equals(workflowName) ? "" : '/' + workflowName);
-    }
-
-    public String getPath() {
-        return sourceControl.toString() + '/' + organization + '/' + repository;
-    }
-
-    @JsonProperty("is_published")
     public boolean isPublished() {
         return isPublished;
     }
 
-    public void setPublished(boolean published) {
-        isPublished = published;
-    }
-
     public long getId() {
         return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setGitUrl(String gitUrl) {
-        this.gitUrl = gitUrl;
-    }
-
-    public String getGitUrl2() {
-        if (gitUrl == null) {
-            return "";
-        }
-        return gitUrl;
     }
 }
