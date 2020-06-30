1c481c3c16d976bc41a275852f51e9bad19f11e8
Charles Overbeck <coverbec@ucsc.edu>
2020-06-30 09:02:29 -0700

    Fix bad merge to master (#3625)
     LGTM check failing because it can't build the previous commit, which this PR fixes.

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/Collection.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/Collection.java
@@ -10,6 +10,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
