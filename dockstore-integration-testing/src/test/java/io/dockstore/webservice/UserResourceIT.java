86b1a47558f5ab870a128a8c7b4a56a3c96dfb4a
Andy Chen <achen81@ucsc.edu>
2020-08-03 11:19:24 -0700

    Updated test to not delete dockstore tokens

--- dockstore-integration-testing/src/test/java/io/dockstore/webservice/UserResourceIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/webservice/UserResourceIT.java
@@ -614,7 +614,7 @@ public class UserResourceIT extends BaseIT {
         io.dockstore.openapi.client.model.Profile userProfile = userApi.getUser().getUserProfiles().get("github.com");
 
         // Delete all of the tokens for every user
-        testingPostgres.runUpdateStatement("DELETE FROM token");
+        testingPostgres.runUpdateStatement("DELETE FROM token WHERE tokensource <> 'dockstore'");
 
         assertNull(userProfile.getEmail());
         assertNull(userProfile.getAvatarURL());
