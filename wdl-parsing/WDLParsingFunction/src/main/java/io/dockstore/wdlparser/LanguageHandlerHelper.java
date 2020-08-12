1da20f978cbec3b3466fb94b35e082c40f8077d5
garyluu <garylau.work@gmail.com>
2020-08-12 18:04:33 -0400

    Add and use format plugin

--- wdl-parsing/WDLParsingFunction/src/main/java/io/dockstore/wdlparser/LanguageHandlerHelper.java
+++ wdl-parsing/WDLParsingFunction/src/main/java/io/dockstore/wdlparser/LanguageHandlerHelper.java
@@ -6,26 +6,27 @@ import java.util.Objects;
 
 public final class LanguageHandlerHelper {
 
-    private LanguageHandlerHelper() {
+  private LanguageHandlerHelper() {}
 
-    }
-    /**
-     * Resolves a relative path based on an absolute parent path
-     * @param parentPath Absolute path to parent file
-     * @param relativePath Relative path the parent file
-     * @return Absolute version of relative path
-     */
-    public static String convertRelativePathToAbsolutePath(String parentPath, String relativePath) {
-        if (relativePath.startsWith("/")) {
-            return relativePath;
-        }
-
-        Path workDir = Paths.get(parentPath);
-
-        // If the workDir is the root, leave it. If it is not the root, set workDir to the parent of parentPath
-        workDir = !Objects.equals(parentPath, workDir.getRoot().toString()) ? workDir.getParent() : workDir;
-
-        return workDir.resolve(relativePath).normalize().toString();
+  /**
+   * Resolves a relative path based on an absolute parent path
+   *
+   * @param parentPath Absolute path to parent file
+   * @param relativePath Relative path the parent file
+   * @return Absolute version of relative path
+   */
+  public static String convertRelativePathToAbsolutePath(String parentPath, String relativePath) {
+    if (relativePath.startsWith("/")) {
+      return relativePath;
     }
 
+    Path workDir = Paths.get(parentPath);
+
+    // If the workDir is the root, leave it. If it is not the root, set workDir to the parent of
+    // parentPath
+    workDir =
+        !Objects.equals(parentPath, workDir.getRoot().toString()) ? workDir.getParent() : workDir;
+
+    return workDir.resolve(relativePath).normalize().toString();
+  }
 }
--- wdl-parsing/WDLParsingFunction/src/main/java/io/dockstore/wdlparser/WDLParserResponse.java
+++ wdl-parsing/WDLParsingFunction/src/main/java/io/dockstore/wdlparser/WDLParserResponse.java
@@ -3,32 +3,32 @@ package io.dockstore.wdlparser;
 import java.util.List;
 
 public class WDLParserResponse {
-    public List<String> getSecondaryFilePaths() {
-        return secondaryFilePaths;
-    }
+  public List<String> getSecondaryFilePaths() {
+    return secondaryFilePaths;
+  }
 
-    public void setSecondaryFilePaths(List<String> secondaryFilePaths) {
-        this.secondaryFilePaths = secondaryFilePaths;
-    }
+  public void setSecondaryFilePaths(List<String> secondaryFilePaths) {
+    this.secondaryFilePaths = secondaryFilePaths;
+  }
 
-    private List<String> secondaryFilePaths;
-    private boolean isValid;
+  private List<String> secondaryFilePaths;
+  private boolean isValid;
 
-    public boolean isValid() {
-        return isValid;
-    }
+  public boolean isValid() {
+    return isValid;
+  }
 
-    public void setValid(boolean valid) {
-        isValid = valid;
-    }
+  public void setValid(boolean valid) {
+    isValid = valid;
+  }
 
-    public String getClonedRepositoryAbsolutePath() {
-        return clonedRepositoryAbsolutePath;
-    }
+  public String getClonedRepositoryAbsolutePath() {
+    return clonedRepositoryAbsolutePath;
+  }
 
-    public void setClonedRepositoryAbsolutePath(String clonedRepositoryAbsolutePath) {
-        this.clonedRepositoryAbsolutePath = clonedRepositoryAbsolutePath;
-    }
+  public void setClonedRepositoryAbsolutePath(String clonedRepositoryAbsolutePath) {
+    this.clonedRepositoryAbsolutePath = clonedRepositoryAbsolutePath;
+  }
 
-    private String clonedRepositoryAbsolutePath;
+  private String clonedRepositoryAbsolutePath;
 }
