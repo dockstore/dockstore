a445590694047efe301d89a063b05130fc6a19ba
garyluu <garylau.work@gmail.com>
2020-08-17 16:39:55 -0400

    Add a test

---
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/GitHubHelperTest.java
@@ -0,0 +1,27 @@
+package io.dockstore.webservice.helpers;
+
+import java.io.IOException;
+
+import io.dockstore.webservice.core.LicenseInformation;
+import org.junit.Assert;
+import org.junit.Test;
+import org.kohsuke.github.AbuseLimitHandler;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.kohsuke.github.RateLimitHandler;
+
+public class GitHubHelperTest {
+
+    @Test
+    public void testGitHubLicense() throws IOException {
+        GitHub gitHub = new GitHubBuilder().withRateLimitHandler(RateLimitHandler.WAIT).withAbuseLimitHandler(
+                AbuseLimitHandler.WAIT).build();
+        LicenseInformation licenseInformation = GitHubHelper.getLicenseInformation(gitHub, "dockstore/lambda");
+        Assert.assertEquals("Apache License 2.0", licenseInformation.getLicenseName());
+        Assert.assertTrue(licenseInformation.getLicenseContent().contains("http://www.apache.org/licenses/"));
+
+        licenseInformation = GitHubHelper.getLicenseInformation(gitHub, "dockstore/dockstore");
+        Assert.assertEquals("Other", licenseInformation.getLicenseName());
+        Assert.assertTrue(licenseInformation.getLicenseContent().contains("http://www.apache.org/licenses/"));
+    }
+}
