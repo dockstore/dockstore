201cde38d240d28490904d1efb477d2d09112962
garyluu <garylau.work@gmail.com>
2020-09-01 15:52:25 -0400

    Use new iteration of TRS

---
+++ dockstore-webservice/src/main/java/io/openapi/api/impl/ServiceInfoApiServiceImpl.java
@@ -0,0 +1,77 @@
+package io.openapi.api.impl;
+
+import java.util.Date;
+import java.util.Optional;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import io.dockstore.webservice.core.User;
+import io.openapi.api.NotFoundException;
+import io.openapi.api.ServiceInfoApiService;
+import io.openapi.model.Service;
+import io.openapi.model.ServiceType;
+
+public class ServiceInfoApiServiceImpl extends ServiceInfoApiService {
+    @Override
+    public Response getServiceInfo(SecurityContext securityContext, ContainerRequestContext value, Optional<User> user)
+            throws NotFoundException {
+        Service service = getService();
+        return Response.ok().entity(service).build();
+    }
+
+    private static Service getService() {
+        Service service = new Service();
+        service.setId("1");
+        service.setName("Dockstore");
+        service.setType(getServiceType());
+        service.setDescription("The GA4GH Tool Registry API is a standard for listing and describing available tools (both stand-alone, Docker-based tools as well as workflows in CWL, WDL, Nextflow, or Galaxy) in a given registry,");
+        service.setOrganization(getOrganization());
+        service.setContactUrl("https://discuss.dockstore.org/t/opening-helpdesk-tickets/1506");
+        service.setDocumentationUrl("https://docs.dockstore.org/en/develop");
+        service.setCreatedAt(new Date());
+        service.setUpdatedAt(new Date());
+        service.setEnvironment("prod");
+        return service;
+    }
+
+    private static ServiceType getServiceType() {
+        ServiceType serviceType = new ServiceType();
+        serviceType.setGroup("org.ga4gh");
+        serviceType.setArtifact("TRS");
+        serviceType.setVersion("2.0.1");
+        return serviceType;
+    }
+
+    private static Organization getOrganization() {
+        Organization organization = new Organization();
+        organization.setName("Dockstore");
+        organization.setUrl("https://dockstore.org");
+        return organization;
+
+    }
+
+    // TODO: Figure out why Swagger codegen couldn't auto-generate an anonymous class (maybe it's just not possible?)
+    static class Organization {
+        String name;
+        String url;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+}
