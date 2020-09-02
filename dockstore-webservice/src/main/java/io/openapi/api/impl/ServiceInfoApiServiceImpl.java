ad2997d7a53e36501cc6ddca8362e4b4ff2a6e5d
garyluu <garylau.work@gmail.com>
2020-09-02 12:01:31 -0400

    Minor changes, actually set prod/dev, use artifact version

--- dockstore-webservice/src/main/java/io/openapi/api/impl/ServiceInfoApiServiceImpl.java
+++ dockstore-webservice/src/main/java/io/openapi/api/impl/ServiceInfoApiServiceImpl.java
@@ -11,6 +11,9 @@ import io.dockstore.webservice.core.User;
 import io.openapi.api.ServiceInfoApiService;
 import io.openapi.model.Service;
 import io.openapi.model.ServiceType;
+import io.swagger.api.impl.ToolsApiServiceImpl;
+
+import static io.dockstore.common.PipHelper.DEV_SEM_VER;
 
 public class ServiceInfoApiServiceImpl extends ServiceInfoApiService {
     @Override
@@ -24,16 +27,19 @@ public class ServiceInfoApiServiceImpl extends ServiceInfoApiService {
         service.setId("1");
         service.setName("Dockstore");
         service.setType(getServiceType());
-        service.setDescription("The GA4GH Tool Registry API is a standard for listing and describing available tools (both stand-alone, Docker-based tools as well as workflows in CWL, WDL, Nextflow, or Galaxy) in a given registry,");
+        service.setDescription("Dockstore is an implementation of the GA4GH Tool Registry API which is a standard for listing and describing available tools (both stand-alone, Docker-based tools as well as workflows in CWL, WDL, Nextflow, or Galaxy)");
         service.setOrganization(getOrganization());
         service.setContactUrl("https://discuss.dockstore.org/t/opening-helpdesk-tickets/1506");
-        service.setDocumentationUrl("https://docs.dockstore.org/en/develop");
+        service.setDocumentationUrl("https://docs.dockstore.org");
         // TODO: Somehow get swagger codegen to generate string parameter (RFC 3339) instead of Date
         service.setCreatedAt(new Date());
         // TODO: Somehow get swagger codegen to generate string parameter (RFC 3339) instead of Date
         service.setUpdatedAt(new Date());
-        service.setEnvironment("prod");
-        service.setVersion("2.0.1");
+        String implVersion = ToolsApiServiceImpl.class.getPackage().getImplementationVersion();
+        String environment = implVersion == null ? "dev" : "prod";
+        implVersion = implVersion == null ? DEV_SEM_VER : implVersion;
+        service.setEnvironment(environment);
+        service.setVersion(implVersion);
         return service;
     }
 
