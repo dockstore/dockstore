03fa9c6a95b32e142a168f0606ec6a96decd0925
Nina Zhao <nina.r.zhao@gmail.com>
2020-09-15 16:58:23 -0400

    wrote test to assert it returns 400

--- dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/GitHubHelperTest.java
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/GitHubHelperTest.java
@@ -2,6 +2,7 @@ package io.dockstore.webservice.helpers;
 
 import java.io.IOException;
 
+import io.dockstore.webservice.CustomWebApplicationException;
 import io.dockstore.webservice.core.LicenseInformation;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,6 +12,20 @@ import org.kohsuke.github.GitHubBuilder;
 import org.kohsuke.github.RateLimitHandler;
 
 public class GitHubHelperTest {
+
+    private static final String CODE = "abcdefghijklmnop";
+    private static final String GITHUB_CLIENT_ID = "123456789abcd";
+    private static final String GITHUB_CLIENT_SECRET = "98654321dcba";
+
+    @Test
+    public void testGetGitHubAccessToken() {
+        try {
+            GitHubHelper.getGitHubAccessToken(CODE, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
+        } catch (CustomWebApplicationException e) {
+            Assert.assertEquals("HTTP 400 Bad Request", e.getMessage());
+        }
+
+    }
 
     // TODO: Replace repos with something less likely to change
     @Test
