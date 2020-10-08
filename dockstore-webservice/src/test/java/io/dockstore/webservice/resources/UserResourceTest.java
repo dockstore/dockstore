5873f8a0df9d66f7d7a62c3995f9337276dfe5cd
NatalieEO <nperez9@ucsc.edu>
2020-10-08 10:57:23 -0700

    don't let users include certain words during user rename (#3843)
    * don't let users include certain words during user rename
    https://ucsc-cgl.atlassian.net/browse/SEAB-1800

---
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/resources/UserResourceTest.java
@@ -0,0 +1,26 @@
+package io.dockstore.webservice.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.dockstore.webservice.CustomWebApplicationException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserResourceTest {
+
+    @Test
+    public void usernameRestrictionsTest() {
+        List<String> invalidUsernames = new ArrayList<>(List.of("dockstore", "DOCKSTORE", "thisIsDoCkStOrE", "admin", "aaaaadminbbb", "curator",
+                "asdflkCURATORSrrr", "system", "SYSTEMMMMMM", "manager", "withoutaMANAGER"));
+        for (String username : invalidUsernames) {
+            try {
+                UserResource.restrictUsername(username);
+                Assert.fail("Should not be able to create a username with a keyword.");
+            } catch (CustomWebApplicationException ex) {
+                Assert.assertTrue(ex.errorMessage.contains("because it contains one or more of the following keywords:"));
+            }
+        }
+    }
+
+}
