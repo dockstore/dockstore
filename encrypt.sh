37bbf53761803656d1c00bed6b2c41406931e6a4
Gary Luu <garylau.work@gmail.com>
2020-11-23 18:34:34 -0500

    Shell check, add comments

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# This script is used to encrypt the confidential test data
+# You will need the environment variables CIRCLE_CI_KEY and CIRCLE_CI_IV populated
+
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -6,6 +9,7 @@ set -o xtrace
 
 tar cvf secrets.tar dockstore-integration-testing/src/test/resources/dstesting_pcks8.pem dockstore-integration-testing/src/test/resources/config_file.txt dockstore-integration-testing/src/test/resources/config_file2.txt dockstore-webservice/src/main/resources/migrations.test.confidential2.xml dockstore-integration-testing/src/test/resources/dockstoreTest.yml dockstore-webservice/src/main/resources/migrations.test.confidential1.xml dockstore-webservice/src/main/resources/migrations.test.confidential1_1.5.0.xml dockstore-webservice/src/main/resources/migrations.test.confidential2_1.5.0.xml
 
-openssl aes-256-cbc -e -in secrets.tar -out circle_ci_test_data.zip.enc -k $CIRCLE_CI_KEY -iv $CIRCLE_CI_IV
+openssl aes-256-cbc -e -in secrets.tar -out circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY" -iv "$CIRCLE_CI_IV"
 
 rm secrets.tar
+
--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-# This script decrypts our test database 
-# WARNING: Edit decrypt.template.mustache not decrypt.sh which is a generated file
+# This script decrypts our test database
 # Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+# You will need the environment variables CIRCLE_CI_KEY and CIRCLE_CI_IV
 
 set -o errexit
 set -o pipefail
 set -o nounset
 set -o xtrace
 
-openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k $CIRCLE_CI_KEY -iv $CIRCLE_CI_IV -out secrets.tar
+openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY" -iv "$CIRCLE_CI_IV" -out secrets.tar
 tar xvf secrets.tar
-#mv dockstore-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
+
