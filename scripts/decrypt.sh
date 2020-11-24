c6062afe83580f206805528682e85db93dca0e4a
Gary Luu <garylau.work@gmail.com>
2020-11-23 19:09:47 -0500

    Upload partial non-confidential Dropwizard yml file

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -10,4 +10,7 @@ set -o xtrace
 
 openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY" -iv "$CIRCLE_CI_IV" -out secrets.tar
 tar xvf secrets.tar
+sudo mkdir -p /home/travis
+sudo cp dockstore-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
+cat dockstore-integration-testing/src/test/resources/partialDockstoreTest.yml >> dockstore-integration-testing/src/test/resources/dockstoreTest.yml
 
