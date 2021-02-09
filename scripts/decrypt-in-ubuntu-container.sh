53649b822b87e1cc1149a01cb88e376a233ee911
NatalieEO <nperez9@ucsc.edu>
2021-02-09 15:21:23 -0800

    decrypt alternative for MacOS

---
+++ scripts/decrypt-in-ubuntu-container.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# This script decrypts our test database
+# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+# You will need the environment variables CIRCLE_CI_KEY and CIRCLE_CI_IV
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+: "$CIRCLE_CI_KEY"
+: "$CIRCLE_CI_IV"
+
+apt update
+apt-get install openssl -y
+cd /home/project
+
+openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY" -iv "$CIRCLE_CI_IV" -out secrets.tar
+tar xvf secrets.tar
+mkdir -p /home/travis
+cp dockstore-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
+cat dockstore-integration-testing/src/test/resources/partialDockstoreTest.yml >> dockstore-integration-testing/src/test/resources/dockstoreTest.yml
+rm secrets.tar
