10f2ab789de903249b1c456c86193d315ee48156
NatalieEO <nperez9@ucsc.edu>
2021-02-16 13:26:41 -0800

    Seab 2194/decrypt script for mac os (#4034)
    https://ucsc-cgl.atlassian.net/browse/SEAB-2194

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -2,6 +2,7 @@
 # This script decrypts our test database
 # Bash3 Boilerplate. Copyright (c) 2014, kvz.io
 # You will need the environment variables CIRCLE_CI_KEY and CIRCLE_CI_IV
+# This is tested with openssl 1.1.1i when running locally. Your mileage may vary with other versions
 
 set -o errexit
 set -o pipefail
@@ -13,8 +14,8 @@ set -o xtrace
 
 openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY" -iv "$CIRCLE_CI_IV" -out secrets.tar
 tar xvf secrets.tar
-sudo mkdir -p /home/travis
-sudo cp dockstore-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
+sudo mkdir -p /usr/local/ci
+sudo cp dockstore-integration-testing/src/test/resources/dstesting_pcks8.pem /usr/local/ci/dstesting_pcks8.pem
 cat dockstore-integration-testing/src/test/resources/partialDockstoreTest.yml >> dockstore-integration-testing/src/test/resources/dockstoreTest.yml
 rm secrets.tar
 
