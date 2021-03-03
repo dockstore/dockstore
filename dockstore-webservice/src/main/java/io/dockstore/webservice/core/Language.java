f488c3fa966f3d749fafb2b16bb7adcd347aa847
Gary Luu <garylau.work@gmail.com>
2021-03-03 12:38:32 -0500

    Make language an enum

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/Language.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/Language.java
@@ -4,14 +4,19 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
+import io.dockstore.common.DescriptorLanguage;
 import io.swagger.annotations.ApiModelProperty;
 
 @Embeddable
 public class Language implements Serializable {
+    // TODO: This needs to be an enum
     @Column(name = "language", nullable = false, columnDefinition = "varchar(50)")
     @ApiModelProperty(value = "The supported language")
-    private String language;
+    @Enumerated(EnumType.STRING)
+    private DescriptorLanguage language;
 
     @Column(name = "version", nullable = true, columnDefinition = "varchar(50)")
     @ApiModelProperty(value = "The version of the supported language")
