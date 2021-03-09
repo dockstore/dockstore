3631e339611ef25506cd21c11fd006b0d1495632
Walter Shands <jshands@ucsc.edu>
2021-03-09 07:09:02 -0800

    for merge of hotfix 1.10.2 to develop (#4086)
    * [maven-release-plugin] prepare release 1.10.2
    * [maven-release-plugin] prepare for next development iteration
    
    Co-authored-by: Denis Yuen <denis.yuen@oicr.on.ca>
    Co-authored-by: Gary Luu <garylau.work@gmail.com>
    Co-authored-by: NatalieEO <nperez9@ucsc.edu>
    Co-authored-by: Richard-Hansen <36607471+Richard-Hansen@users.noreply.github.com>

--- dockstore-integration-testing/src/test/java/io/dockstore/webservice/languages/GalaxyPluginIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/webservice/languages/GalaxyPluginIT.java
@@ -121,6 +121,8 @@ public class GalaxyPluginIT {
         }
     };
 
+    private final String galaxyWorkflowRepo = "DockstoreTestUser2/workflow-testing-repo";
+    private final String installationId = "1179416";
     private FileDAO fileDAO;
 
     @BeforeClass
@@ -203,12 +205,9 @@ public class GalaxyPluginIT {
     public void testTestParameterPaths() {
         final ApiClient webClient = getWebClient(true, BaseIT.USER_2_USERNAME, testingPostgres);
         WorkflowsApi workflowApi = new WorkflowsApi(webClient);
-
-        String galaxyWorkflowRepo = "DockstoreTestUser2/workflow-testing-repo";
-        String installationId = "1179416";
-        List<Workflow> workflows = workflowApi.handleGitHubRelease(galaxyWorkflowRepo, BaseIT.USER_2_USERNAME, "refs/tags/dockstore/3851",
-                installationId);
-        WorkflowVersion version = workflows.get(0).getWorkflowVersions().get(0);
+        workflowApi.handleGitHubRelease(galaxyWorkflowRepo, BaseIT.USER_2_USERNAME, "refs/tags/dockstore/3851", installationId);
+        Workflow workflow = workflowApi.getWorkflowByPath("github.com/" + galaxyWorkflowRepo + "/COVID-19 variation analysis on Illumina metagenomic data", "", false);
+        WorkflowVersion version = workflow.getWorkflowVersions().get(0);
         List<SourceFile> sourceFiles = fileDAO.findSourceFilesByVersion(version.getId());
         assertTrue("Test file should have the expected path",
                 sourceFiles.stream().anyMatch(sourceFile -> sourceFile.getPath().endsWith("/workflow-test.yml")));
