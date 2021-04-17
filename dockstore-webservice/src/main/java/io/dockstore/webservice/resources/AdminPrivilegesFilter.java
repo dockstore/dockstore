dfa7aff5756cda9ef1d06eb7e8ce4133d30b2c90
Charles Overbeck <coverbec@ucsc.edu>
2021-04-16 18:18:59 -0700

    Only log privileged admin actions
    Groundwork for https://ucsc-cgl.atlassian.net/browse/SEAB-2152
    
    which requests an alert for privileged actions; currently it
    is logging all requests, and is very noisy.

--- dockstore-webservice/src/main/java/io/dockstore/webservice/resources/AdminPrivilegesFilter.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/resources/AdminPrivilegesFilter.java
@@ -1,34 +1,55 @@
 package io.dockstore.webservice.resources;
 
+import java.lang.reflect.Method;
 import java.security.Principal;
+import java.util.Arrays;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 
+import io.dockstore.webservice.SimpleAuthorizer;
 import io.dockstore.webservice.core.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Every request will be filtered through here to determine if an admin user is making the request. If it is an admin,
- * then the username, method, and endpoint path will be logged.
+ * and it is a request that requires admin privileges, then the username, method, and endpoint path will be logged.
  */
 @Provider
 public class AdminPrivilegesFilter implements ContainerRequestFilter {
     private static final Logger LOG = LoggerFactory.getLogger(AdminPrivilegesFilter.class);
 
+    @Context
+    private ResourceInfo resourceInfo;
+
     @Override
     public void filter(ContainerRequestContext requestContext) {
         if (requestContext != null) {
-            Principal principal = requestContext.getSecurityContext().getUserPrincipal();
+            final Principal principal = requestContext.getSecurityContext().getUserPrincipal();
             if (principal instanceof User) {
-                User user = (User)principal;
-                if (user.getIsAdmin()) {
-                    String logMessage = "Admin " + user.getUsername() + " making " + requestContext.getMethod() + " request at " + requestContext.getUriInfo().getPath();
+                final User user = (User)principal;
+                if (user.getIsAdmin() && shouldLog()) {
+                    final String logMessage = "Admin " + user.getUsername() + " making " + requestContext.getMethod()
+                            + " privileged request at " + requestContext.getUriInfo().getPath();
                     LOG.info(logMessage);
                 }
             }
         }
+    }
+
+    private boolean shouldLog() {
+        final Method resourceMethod = resourceInfo.getResourceMethod();
+        if (resourceMethod != null) { // JavaDoc says it can be null, hmm
+            final RolesAllowed rolesAllowedAnnotation = resourceMethod.getAnnotation(RolesAllowed.class);
+            if (rolesAllowedAnnotation != null) {
+                return Arrays.stream(rolesAllowedAnnotation.value()).anyMatch(role -> SimpleAuthorizer.ADMIN.equals(role));
+            }
+        }
+        return false;
     }
 }
