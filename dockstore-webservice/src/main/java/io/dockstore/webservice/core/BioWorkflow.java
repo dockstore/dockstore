38a57cd3db339b91795c37d8824b1f34265d312b
Gary Luu <garylau.work@gmail.com>
2021-05-05 13:40:13 -0400

    Change is_checker to isChecker

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/BioWorkflow.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/BioWorkflow.java
@@ -56,7 +56,6 @@ public class BioWorkflow extends Workflow {
     private Entry parentEntry;
 
     @Column(columnDefinition = "boolean default false")
-    @JsonProperty("is_checker")
     @ApiModelProperty(position = 23)
     private boolean isChecker = false;
 
