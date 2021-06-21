7dd370168f1cc840cc0a7002a5395940dd77a17f
Gary Luu <garylau.work@gmail.com>
2021-06-21 16:48:48 -0400

    Move sam cli install

--- scripts/install-tests.sh
+++ scripts/install-tests.sh
@@ -6,19 +6,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 set -o xtrace
-if [ "${TESTING_PROFILE}" = "language-parsing-tests" ]; then
-    pip3 install aws-sam-cli
-    sudo apt install unzip
-    wget https://github.com/dockstore/lambda/releases/download/0.1.6-SNAPSHOT/wdl-parsing.zip
-    unzip wdl-parsing.zip
-    cd wdl-parsing
-    # docker pull amazon/aws-sam-cli-build-image-java11
-    # cannot seem to `sam build --use-container` using local jdk and mvn instead
-    sam build
-    sam local start-api &
-    cd ..
-    exit 0;
-fi
 if [ "${TESTING_PROFILE}" = "unit-tests" ] || [ "${TESTING_PROFILE}" == "automated-review" ]; then
     exit 0;
 fi
