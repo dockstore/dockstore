fa6090c0184eb90dcc7cb3f0813059420d560feb
Kathy Tran <kathylevtran@gmail.com>
2021-07-22 14:58:18 -0400

    Add UCSC to Copyright text

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/docker/DockerPlatformManifest.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/docker/DockerPlatformManifest.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2021 OICR
+ *    Copyright 2021 OICR and UCSC
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
