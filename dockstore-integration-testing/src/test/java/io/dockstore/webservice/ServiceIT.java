93a88e0b056b2a0b2b4ae516a169f5ef79d93050
Denis Yuen <denis.yuen@oicr.on.ca>
2021-08-11 16:28:07 -0400

    Feature/postgres 13 theory (#4396)
    postgres 13

--- dockstore-integration-testing/src/test/java/io/dockstore/webservice/ServiceIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/webservice/ServiceIT.java
@@ -55,6 +55,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import org.apache.http.HttpStatus;
+import org.glassfish.jersey.client.ClientProperties;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -178,7 +179,7 @@ public class ServiceIT extends BaseIT {
      * @param path         Path of endpoint
      */
     private void testXTotalCount(Client jerseyClient, String path) {
-        Response response = jerseyClient.target(path).request().get();
+        Response response = jerseyClient.target(path).request().property(ClientProperties.READ_TIMEOUT, 0).get();
         assertEquals(HttpStatus.SC_OK, response.getStatus());
         MultivaluedMap<String, Object> headers = response.getHeaders();
         Object xTotalCount = headers.getFirst("X-total-count");
