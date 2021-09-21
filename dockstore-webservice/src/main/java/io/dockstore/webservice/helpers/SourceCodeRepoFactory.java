cb70c4ed46bd688789514fc060c4edfbf991a685
Walt Shands <jshands@ucsc.edu>
2021-09-21 16:44:04 -0700

    fix redos vulnerable regex expressions

--- dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/SourceCodeRepoFactory.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/SourceCodeRepoFactory.java
@@ -120,10 +120,15 @@ public final class SourceCodeRepoFactory {
      */
     public static Map<String, String> parseGitUrl(String url) {
         // format 1 git@github.com:dockstore/dockstore-ui.git
-        Pattern p1 = Pattern.compile("git\\@(\\S+):(\\S+)/(\\S+)\\.git");
+        // Avoid SonarCloud warning: Using slow regular expressions is security-sensitive
+        // https://sonarcloud.io/organizations/dockstore/rules?open=java%3AS5852&rule_key=java%3AS5852
+        // See Prevent Catastrophic Backtracking and Possessive Quantifiers and Atomic Grouping to The Rescue
+        // in https://www.regular-expressions.info/catastrophic.html
+        // So use more restrictive regex and possesive quantifiers '++'
+        Pattern p1 = Pattern.compile("git@([^\\s:/]++):([^\\s:/]++)/([^\\s:/.]++)\\.git");
         Matcher m1 = p1.matcher(url);
         // format 2 git://github.com/denis-yuen/dockstore-whalesay.git (should be avoided)
-        Pattern p2 = Pattern.compile("git://(\\S+)/(\\S+)/(\\S+)\\.git");
+        Pattern p2 = Pattern.compile("git:/([^\\s:/]++)/([^\\s:/]++)/([^\\s:/.]++)\\.git");
         Matcher m2 = p2.matcher(url);
 
         Matcher matcherActual;
