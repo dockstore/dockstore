918bf99ccaf9b2a897d5e3f9af98639644ad8e30
Walt Shands <jshands@ucsc.edu>
2021-09-24 17:54:25 -0700

    clean up tests

--- dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/SourceCodeRepoFactoryTest.java
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/SourceCodeRepoFactoryTest.java
@@ -29,35 +29,34 @@ public class SourceCodeRepoFactoryTest {
     public void parseGitUrl() {
         // https://stackoverflow.com/questions/59081778/rules-for-special-characters-in-github-repository-name
         // test format 1
-        final Map<String, String> stringStringMap1 = SourceCodeRepoFactory.parseGitUrl("git@github.com:dockstore/dockstore-ui.git");
+        final Map<String, String> stringStringMap1 = SourceCodeRepoFactory
+                .parseGitUrl("git@github.com:dockstore/dockstore-ui.git");
         Assert.assertNotNull(stringStringMap1);
-        Assert.assertTrue("values not found",
-                stringStringMap1.containsKey("Source") && stringStringMap1.get("Source").equals("github.com")
-                        && stringStringMap1.containsKey("Username") && stringStringMap1.get("Username").equals("dockstore")
-                        && stringStringMap1.containsKey("Repository") && stringStringMap1.get("Repository").equals("dockstore-ui"));
+        Assert.assertEquals("github.com", stringStringMap1.get("Source"));
+        Assert.assertEquals("dockstore", stringStringMap1.get("Username"));
+        Assert.assertEquals("dockstore-ui", stringStringMap1.get("Repository"));
 
-        final Map<String, String> repoMapWithPeriodAndHyphen = SourceCodeRepoFactory.parseGitUrl("git@github.com:DockstoreTestUser2/wdl-1.0-work_flow.git");
+        final Map<String, String> repoMapWithPeriodAndHyphen = SourceCodeRepoFactory
+                .parseGitUrl("git@github.com:DockstoreTestUser2/wdl-1.0-work_flow.git");
         Assert.assertNotNull(repoMapWithPeriodAndHyphen);
-        Assert.assertTrue("values not found",
-                repoMapWithPeriodAndHyphen.containsKey("Source") && repoMapWithPeriodAndHyphen.get("Source").equals("github.com")
-                        && repoMapWithPeriodAndHyphen.containsKey("Username") && repoMapWithPeriodAndHyphen.get("Username").equals("DockstoreTestUser2")
-                        && repoMapWithPeriodAndHyphen.containsKey("Repository") && repoMapWithPeriodAndHyphen.get("Repository").equals("wdl-1.0-work_flow"));
+        Assert.assertEquals("github.com", repoMapWithPeriodAndHyphen.get("Source"));
+        Assert.assertEquals("DockstoreTestUser2", repoMapWithPeriodAndHyphen.get("Username"));
+        Assert.assertEquals("wdl-1.0-work_flow", repoMapWithPeriodAndHyphen.get("Repository"));
 
         // test format 2
         final Map<String, String> stringStringMap2 = SourceCodeRepoFactory
                 .parseGitUrl("git://github.com/denis-yuen/dockstore-whalesay.git");
         Assert.assertNotNull(stringStringMap2);
-        Assert.assertTrue("values not found",
-                stringStringMap2.containsKey("Source") && stringStringMap2.get("Source").equals("github.com")
-                        && stringStringMap2.containsKey("Username") && stringStringMap2.get("Username").equals("denis-yuen")
-                        && stringStringMap2.containsKey("Repository") && stringStringMap2.get("Repository").equals("dockstore-whalesay"));
+        Assert.assertEquals("github.com", stringStringMap2.get("Source"));
+        Assert.assertEquals("denis-yuen", stringStringMap2.get("Username"));
+        Assert.assertEquals("dockstore-whalesay", stringStringMap2.get("Repository"));
 
-        final Map<String, String> repoMapWithPeriodAndHyphen2 = SourceCodeRepoFactory.parseGitUrl("git://github.com/DockstoreTestUser2/wdl-1.0-work_flow.git");
+        final Map<String, String> repoMapWithPeriodAndHyphen2 = SourceCodeRepoFactory
+                .parseGitUrl("git://github.com/DockstoreTestUser2/wdl-1.0-work_flow.git");
         Assert.assertNotNull(repoMapWithPeriodAndHyphen2);
-        Assert.assertTrue("values not found",
-                repoMapWithPeriodAndHyphen2.containsKey("Source") && repoMapWithPeriodAndHyphen2.get("Source").equals("github.com")
-                        && repoMapWithPeriodAndHyphen2.containsKey("Username") && repoMapWithPeriodAndHyphen2.get("Username").equals("DockstoreTestUser2")
-                        && repoMapWithPeriodAndHyphen2.containsKey("Repository") && repoMapWithPeriodAndHyphen2.get("Repository").equals("wdl-1.0-work_flow"));
+        Assert.assertEquals("github.com", repoMapWithPeriodAndHyphen2.get("Source"));
+        Assert.assertEquals("DockstoreTestUser2", repoMapWithPeriodAndHyphen2.get("Username"));
+        Assert.assertEquals("wdl-1.0-work_flow", repoMapWithPeriodAndHyphen2.get("Repository"));
 
         // test garbage
         final Map<String, String> stringStringMap3 = SourceCodeRepoFactory.parseGitUrl("mostly harmless");
