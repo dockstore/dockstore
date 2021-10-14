5b5208094ea74164b154a78a09f37c082054abfe
Kathy Tran <kathylevtran@gmail.com>
2021-10-14 17:01:25 -0400

    Fix tests

--- dockstore-integration-testing/src/test/java/io/dockstore/client/cli/LimitedCRUDClientIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/client/cli/LimitedCRUDClientIT.java
@@ -148,7 +148,6 @@ public class LimitedCRUDClientIT {
         // clear lazy fields for now till merge
         hostedTool.setAliases(null);
         container.setAliases(null);
-        hostedTool.setUserIdToOrcidPutCode(null); // Setting it to null to compare with the getContainer endpoint since that one doesn't return orcid put codes
         assertEquals(container, hostedTool);
         assertNull(container.getUsers());
 
