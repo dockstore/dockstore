d43a5d8421cde7c949ce68fe4241a56ff69b43e5
Denis Yuen <denis.yuen@oicr.on.ca>
2022-02-14 15:30:00 -0500

    Merge tag '1.11.12' into develop
    [maven-release-plugin] copy for tag 1.11.12

--- dockstore-webservice/src/main/java/io/dockstore/webservice/jdbi/BioWorkflowDAO.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/jdbi/BioWorkflowDAO.java
@@ -17,11 +17,18 @@ package io.dockstore.webservice.jdbi;
 
 import static io.dockstore.webservice.resources.MetadataResource.RSS_ENTRY_LIMIT;
 
+import io.dockstore.common.DescriptorLanguage;
 import io.dockstore.webservice.core.BioWorkflow;
+import io.dockstore.webservice.core.SourceControlConverter;
 import io.dockstore.webservice.core.database.MyWorkflows;
 import io.dockstore.webservice.core.database.RSSWorkflowPath;
 import io.dockstore.webservice.core.database.WorkflowPath;
 import java.util.List;
+import java.util.Optional;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import org.hibernate.SessionFactory;
 
 /**
@@ -31,6 +38,36 @@ import org.hibernate.SessionFactory;
 public class BioWorkflowDAO extends EntryDAO<BioWorkflow> {
     public BioWorkflowDAO(SessionFactory factory) {
         super(factory);
+    }
+
+    @Override
+    @SuppressWarnings({"checkstyle:ParameterNumber"})
+    protected Root<BioWorkflow> generatePredicate(DescriptorLanguage descriptorLanguage, String registry, String organization, String name, String toolname, String description, String author, Boolean checker,
+        CriteriaBuilder cb, CriteriaQuery<?> q) {
+
+        final SourceControlConverter converter = new SourceControlConverter();
+        final Root<BioWorkflow> entryRoot = q.from(BioWorkflow.class);
+
+        Predicate predicate = cb.isTrue(entryRoot.get("isPublished"));
+        predicate = andLike(cb, predicate, entryRoot.get("organization"), Optional.ofNullable(organization));
+        predicate = andLike(cb, predicate, entryRoot.get("repository"), Optional.ofNullable(name));
+        predicate = andLike(cb, predicate, entryRoot.get("workflowName"), Optional.ofNullable(toolname));
+        predicate = andLike(cb, predicate, entryRoot.get("description"), Optional.ofNullable(description));
+        predicate = andLike(cb, predicate, entryRoot.get("author"), Optional.ofNullable(author));
+
+        if (descriptorLanguage != null) {
+            predicate = cb.and(predicate, cb.equal(entryRoot.get("descriptorType"), descriptorLanguage));
+        }
+        if (registry != null) {
+            predicate = cb.and(predicate, cb.equal(entryRoot.get("sourceControl"), converter.convertToEntityAttribute(registry)));
+        }
+
+        if (checker != null) {
+            predicate = cb.and(predicate, cb.isTrue(entryRoot.get("isChecker")));
+        }
+
+        q.where(predicate);
+        return entryRoot;
     }
 
     public List<WorkflowPath> findAllPublishedPaths() {
