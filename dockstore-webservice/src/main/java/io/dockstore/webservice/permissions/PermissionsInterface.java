95fac05ea7926e3654840509703927ac4477e4f2
David Steinberg <david@resium.com>
2022-04-26 16:10:47 -0700

    Roll back changing import statement

--- dockstore-webservice/src/main/java/io/dockstore/webservice/permissions/PermissionsInterface.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/permissions/PermissionsInterface.java
@@ -21,7 +21,10 @@ import io.dockstore.webservice.core.TokenType;
 import io.dockstore.webservice.core.User;
 import io.dockstore.webservice.core.Workflow;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.http.HttpStatus;
 
