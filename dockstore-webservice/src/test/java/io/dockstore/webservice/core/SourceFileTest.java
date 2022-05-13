566f2f63e86db3ca15fba11a7263acaf8fe6c8cc
Stephen Von Worley <svonworl@ucsc.edu>
2022-05-13 14:53:13 -0700

    improve unit test

--- dockstore-webservice/src/test/java/io/dockstore/webservice/core/SourceFileTest.java
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/core/SourceFileTest.java
@@ -1,14 +1,46 @@
 package io.dockstore.webservice.core;
 
 import io.dockstore.webservice.CustomWebApplicationException;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SourceFileTest {
 
-    private static final List<String> GOOD_PATHS = List.of("/", ".", "-", "_", "abcz", "ABCZ", "01239", "/good_path.swl", ".dockstore.yml");
-    private static final List<String> BAD_PATHS = List.of("!", "~", "`", "'", "\n", " ", "$", "*", "\u0080", "/`bad_path`.cwl", "~/.dockstore.yml");
+    private static final List<String> GOOD_PATHS = List.of("/", ".", "-", "_", "abcz", "ABCZ", "01239", "some/good-path/under_score.cwl", ".dockstore.yml");
+
+    /**
+     * Generate a list of characters that are not allowed in SourceFile paths, in the range
+     * from '\0' to an arbitrary character beyond the range of 8-bit ASCII.
+     */
+    private List<Character> computeBadChars() {
+        List<Character> badChars = new ArrayList<>();
+        for (int c = 0; c <= 300; c++) {
+            boolean good =
+                (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')
+                || c == '/' || c == '.' || c == '-' || c == '_';
+            if (!good) {
+                badChars.add((char)c);
+            }
+        }
+        return badChars;
+    }
+
+    private void testBadPath(String badPath) {
+        try {
+            new SourceFile().setPath(badPath);
+            Assert.fail("should have thrown the appropriate exception");
+        } catch (CustomWebApplicationException e) {
+            // expected execution path on successful test
+        }
+        try {
+            new SourceFile().setAbsolutePath(badPath);
+            Assert.fail("should have thrown the appropriate exception");
+        } catch (CustomWebApplicationException e) {
+            // expected execution path on successful test
+        }
+    }
 
     @Test
     public void testSettingPaths() {
@@ -16,20 +48,11 @@ public class SourceFileTest {
         for (String goodPath: GOOD_PATHS) {
             new SourceFile().setPath(goodPath);
             new SourceFile().setAbsolutePath(goodPath);
-        }
 
-        for (String badPath: BAD_PATHS) {
-            try {
-                new SourceFile().setPath(badPath);
-                Assert.fail("should have thrown the appropriate exception");
-            } catch (CustomWebApplicationException e) {
-                // expected execution path on successful test
-            }
-            try {
-                new SourceFile().setAbsolutePath(badPath);
-                Assert.fail("should have thrown the appropriate exception");
-            } catch (CustomWebApplicationException e) {
-                // expected execution path on successful test
+            for (char bad: computeBadChars()) {
+                testBadPath(goodPath + bad);
+                testBadPath(bad + goodPath);
+                testBadPath(goodPath + bad + goodPath);
             }
         }
     }
