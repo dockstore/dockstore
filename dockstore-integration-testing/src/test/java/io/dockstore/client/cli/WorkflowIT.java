100f8fb15601a995321c0882b8e0c153a191a8e2
Stephen Von Worley <svonworl@ucsc.edu>
2022-05-20 21:09:52 -0700

    remove unused import

--- dockstore-integration-testing/src/test/java/io/dockstore/client/cli/WorkflowIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/client/cli/WorkflowIT.java
@@ -88,7 +88,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
