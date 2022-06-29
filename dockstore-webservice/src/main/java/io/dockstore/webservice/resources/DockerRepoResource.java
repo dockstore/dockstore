28a0f8e5b23ce2efea85b6828e3db53b9fcfca34
Charles Overbeck <coverbec@ucsc.edu>
2022-06-28 18:00:29 -0700

    Use versionNameFilter for proprietary API
    #4975

--- dockstore-webservice/src/main/java/io/dockstore/webservice/resources/DockerRepoResource.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/resources/DockerRepoResource.java
@@ -61,6 +61,7 @@ import io.dockstore.webservice.jdbi.TagDAO;
 import io.dockstore.webservice.jdbi.TokenDAO;
 import io.dockstore.webservice.jdbi.ToolDAO;
 import io.dockstore.webservice.jdbi.UserDAO;
+import io.dockstore.webservice.jdbi.VersionDAO;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
 import io.swagger.annotations.Api;
@@ -134,6 +135,7 @@ public class DockerRepoResource
     private final LabelDAO labelDAO;
     private final FileDAO fileDAO;
     private final FileFormatDAO fileFormatDAO;
+    private final VersionDAO versionDAO;
     private final HttpClient client;
     private final String bitbucketClientID;
     private final String bitbucketClientSecret;
@@ -154,6 +156,7 @@ public class DockerRepoResource
         this.fileDAO = new FileDAO(sessionFactory);
         this.eventDAO = new EventDAO(sessionFactory);
         this.fileFormatDAO = new FileFormatDAO(sessionFactory);
+        this.versionDAO = new VersionDAO(sessionFactory);
         this.client = client;
 
         this.bitbucketClientID = configuration.getBitbucketClientID();
@@ -882,7 +885,7 @@ public class DockerRepoResource
     public SourceFile dockerfile(@ApiParam(hidden = true) @Parameter(hidden = true, name = "user")@Auth Optional<User> user,
         @ApiParam(value = "Tool id", required = true) @PathParam("containerId") Long containerId, @QueryParam("tag") String tag) {
 
-        return getSourceFile(containerId, tag, DescriptorLanguage.FileType.DOCKERFILE, user, fileDAO);
+        return getSourceFile(containerId, tag, DescriptorLanguage.FileType.DOCKERFILE, user, fileDAO, versionDAO);
     }
 
     // Add for new descriptor types
@@ -897,7 +900,7 @@ public class DockerRepoResource
     public SourceFile primaryDescriptor(@ApiParam(hidden = true) @Parameter(hidden = true, name = "user")@Auth Optional<User> user,
         @ApiParam(value = "Tool id", required = true) @PathParam("containerId") Long containerId, @QueryParam("tag") String tag, @QueryParam("language") String language) {
         final FileType fileType = DescriptorLanguage.getOptionalFileType(language).orElseThrow(() ->  new CustomWebApplicationException("Language not valid", HttpStatus.SC_BAD_REQUEST));
-        return getSourceFile(containerId, tag, fileType, user, fileDAO);
+        return getSourceFile(containerId, tag, fileType, user, fileDAO, versionDAO);
     }
 
     @GET
@@ -912,7 +915,7 @@ public class DockerRepoResource
         @ApiParam(value = "Tool id", required = true) @PathParam("containerId") Long containerId, @QueryParam("tag") String tag,
         @PathParam("relative-path") String path, @QueryParam("language") String language) {
         final FileType fileType = DescriptorLanguage.getOptionalFileType(language).orElseThrow(() ->  new CustomWebApplicationException("Language not valid", HttpStatus.SC_BAD_REQUEST));
-        return getSourceFileByPath(containerId, tag, fileType, path, user, fileDAO);
+        return getSourceFileByPath(containerId, tag, fileType, path, user, fileDAO, versionDAO);
     }
 
     @GET
@@ -926,7 +929,7 @@ public class DockerRepoResource
     public List<SourceFile> secondaryDescriptors(@ApiParam(hidden = true) @Parameter(hidden = true, name = "user")@Auth Optional<User> user,
         @ApiParam(value = "Tool id", required = true) @PathParam("containerId") Long containerId, @QueryParam("tag") String tag, @QueryParam("language") DescriptorLanguage language) {
         final FileType fileType = language.getFileType();
-        return getAllSecondaryFiles(containerId, tag, fileType, user, fileDAO);
+        return getAllSecondaryFiles(containerId, tag, fileType, user, fileDAO, versionDAO);
     }
 
     @GET
@@ -941,7 +944,7 @@ public class DockerRepoResource
         @ApiParam(value = "Tool id", required = true) @PathParam("containerId") Long containerId, @QueryParam("tag") String tag,
         @ApiParam(value = "Descriptor Type", required = true) @QueryParam("descriptorType") DescriptorLanguage descriptorLanguage) {
         final FileType testParameterType = descriptorLanguage.getTestParamType();
-        return getAllSourceFiles(containerId, tag, testParameterType, user, fileDAO);
+        return getAllSourceFiles(containerId, tag, testParameterType, user, fileDAO, versionDAO);
     }
 
     /**
