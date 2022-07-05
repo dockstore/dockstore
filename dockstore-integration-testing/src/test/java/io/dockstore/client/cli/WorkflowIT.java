869ddcc0698b67e75ee9035269a73bb220bc2168
Stephen Von Worley <svonworl@ucsc.edu>
2022-07-05 16:58:21 -0700

    change return code

--- dockstore-integration-testing/src/test/java/io/dockstore/client/cli/WorkflowIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/client/cli/WorkflowIT.java
@@ -461,7 +461,7 @@ public class WorkflowIT extends BaseIT {
         try {
             anonWorkflowApi.getWorkflowZip(100000000L, 1000000L);
         } catch (ApiException ex) {
-            assertEquals(HttpStatus.SC_BAD_REQUEST, ex.getCode());
+            assertEquals(HttpStatus.SC_NOT_FOUND, ex.getCode());
             success = true;
         }
         assertTrue("should have got " + HttpStatus.SC_BAD_REQUEST, success);
