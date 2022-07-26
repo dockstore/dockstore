330a7af2c8d9deac104fddedb6e49fad3460b653
Denis Yuen <denis.yuen@oicr.on.ca>
2022-07-26 17:41:45 -0400

    fix specific test

--- dockstore-webservice/src/main/java/io/dockstore/webservice/jdbi/VersionDAO.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/jdbi/VersionDAO.java
@@ -59,11 +59,14 @@ public class VersionDAO<T extends Version> extends AbstractDAO<T> {
         return (long)query.getSingleResult();
     }
 
-    // Currently not used, but may be useful later for archive/deletion operations
     public long getVersionsFrozen(long entryId) {
         Query query = namedQuery("io.dockstore.webservice.core.Version.getCountVersionFrozenByEntryID");
         query.setParameter("id", entryId);
-        return (long) query.getSingleResult();
+        final Object singleResult = query.getSingleResult();
+        if (singleResult == null) {
+            return 0;
+        }
+        return (long)singleResult;
     }
 
     public void enableNameFilter(String name) {
