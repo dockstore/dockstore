83d6968a2573f138e0d3080976ae4650b0408026
Flynn Hembroff <flynnhembroff@gmail.com>
2022-08-02 22:46:52 -0400

    Made it so instructions.md is generated by macos-instructions.sh and macos_instructions.yaml

--- scripts/macos-instructions.sh
+++ scripts/macos-instructions.sh
@@ -2,7 +2,7 @@
 
 
 
-INSTRUCTION_FILE=macos_instructions.json
+INSTRUCTION_FILE=macos_instructions.yaml
 OUTPUT_FILE_TEXT=instructions.md
 OUTPUT_FILE_CODE=instructions.sh
 
@@ -10,32 +10,34 @@ OUTPUT_FILE_CODE=instructions.sh
 > "$OUTPUT_FILE_TEXT"
 > "OUTPUT_FILE_CODE"
 
-NUMBER_OF_INSTRUCTIONS=$(jq '. | length' macos_instructions.json)
+NUMBER_OF_INSTRUCTIONS=$(yq '.setupInformation | length' "$INSTRUCTION_FILE")
 
 
-format_string () {
-  # Remove outer quotations, for example "blue" would become blue
-  STRING_TO_FORMAT=$(sed 's+^\"++;s+\"$++' <<< "$STRING_TO_FORMAT")
-  # Remove \ from all \"
-  STRING_TO_FORMAT=$(sed 's+\\"+"+g' <<< "$STRING_TO_FORMAT")
-}
-
+#format_string () {
+#  # Remove outer quotations, for example "blue" would become blue
+#  STRING_TO_FORMAT=$(sed 's+^\"++;s+\"$++' <<< "$STRING_TO_FORMAT")
+#  # Remove \ from all \"
+#  STRING_TO_FORMAT=$(sed 's+\\"+"+g' <<< "$STRING_TO_FORMAT")
+#}
+export i=0
 for (( i=0; i<"$NUMBER_OF_INSTRUCTIONS"; i++ ))
 do
    # Get key
-   key=$(jq --argjson index "${i}" '.[$index] | keys' macos_instructions.json | tr -cd '[:alnum:]')
+   key=$(yq '.setupInformation.[env(i)] | keys' "$INSTRUCTION_FILE" | tr -cd '[:alnum:]')
    if [ "$key" == "text" ]; then
-      jq --argjson index "${i}" '.[$index].text' macos_instructions.json  | sed 's/^.//;s/.$//' >> $OUTPUT_FILE_TEXT
-   elif [ "$key" == "newline" ]; then
-      echo "" >> $OUTPUT_FILE_TEXT
+      yq  '.setupInformation.[env(i)].text' "$INSTRUCTION_FILE"  >> $OUTPUT_FILE_TEXT
+   elif [ "$key" == "newLine" ]; then
+      NUMBER_LINES_TO_PRINT=$(yq '.setupInformation[env(i)].newLine' "$INSTRUCTION_FILE")
+      yes "" | head -n "$NUMBER_LINES_TO_PRINT" >> "$OUTPUT_FILE_TEXT"
+
    elif [ "$key" == "code" ]; then
-     STRING_TO_FORMAT=$(jq --argjson index "${i}" '.[$index].code.displayBoth' macos_instructions.json)
-     format_string
+     STRING_TO_PRINT=$(yq  '.setupInformation.[env(i)].code.run' "$INSTRUCTION_FILE")
      {
       echo "\`\`\`"
-      echo "$STRING_TO_FORMAT"
+      echo -e "$STRING_TO_PRINT"
       echo "\`\`\`"
      } >> "$OUTPUT_FILE_TEXT"
+
    else
        echo "$key"
        echo "Strings are not equal."
@@ -44,8 +46,8 @@ do
 done
 
 # Delete current instructions from README.md
-sed -n '1,/DO_NOT_DELETE_START_MACOS_INSTRUCTIONS/p;/DO_NOT_DELETE_END_MACOS_INSTRUCTIONS/,$p' README.md
+#sed -n '1,/DO_NOT_DELETE_START_MACOS_INSTRUCTIONS/p;/DO_NOT_DELETE_END_MACOS_INSTRUCTIONS/,$p' README.md
 # Add new instructions to file
-> temp.txt
-sed '/DO_NOT_DELETE_END_MACOS_INSTRUCTIONS/e cat instructions.md' README.md > temp.txt
-cat temp.txt > README.md
+#> temp.txt
+#sed '/DO_NOT_DELETE_END_MACOS_INSTRUCTIONS/e cat instructions.md' README.md > temp.txt
+#cat temp.txt > README.md
