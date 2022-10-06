f927996d369dd7859b6e4d08984a193186ea27d4
Stephen Von Worley <svonworl@ucsc.edu>
2022-10-06 13:13:32 -0700

    include new files

---
+++ dockstore-common/src/main/java/io/dockstore/common/yaml/YamlTool.java
@@ -0,0 +1,11 @@
+// TODO add copyright
+package io.dockstore.common.yaml;
+
+import io.dockstore.common.yaml.constraints.ToolIsCwl;
+
+/**
+ * An apptool as described in a .dockstore.yml
+ */
+@ToolIsCwl
+public class YamlTool extends YamlWorkflow {
+}
