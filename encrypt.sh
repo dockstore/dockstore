a8c0f28faa02ad28af74cfdcfade2ae01da13ecc
Denis Yuen <denis.yuen@oicr.on.ca>
2023-01-05 14:31:34 -0500

    doc [skip ci]

--- encrypt.sh
+++ encrypt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script is used to encrypt the confidential test data
-# You will need the environment variables CIRCLE_CI_KEY_2 and CIRCLE_CI_IV_2 populated
+# You will need the environment variables CIRCLE_CI_WS_KEY and CIRCLE_CI_WS_IV populated
 
 set -o errexit
 set -o pipefail
