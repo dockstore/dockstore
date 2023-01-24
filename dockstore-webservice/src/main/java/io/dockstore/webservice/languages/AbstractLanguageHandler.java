dd077b32bcbba1c0fe9e2a95315638afc67a19a9
Stephen Von Worley <svonworl@ucsc.edu>
2023-01-24 11:38:19 -0800

    revert AbstractLanguageHandler

--- dockstore-webservice/src/main/java/io/dockstore/webservice/languages/AbstractLanguageHandler.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/languages/AbstractLanguageHandler.java
@@ -4,10 +4,7 @@ import io.dockstore.common.DescriptorLanguage;
 import io.dockstore.webservice.core.SourceFile;
 import io.dockstore.webservice.core.Version;
 import io.dockstore.webservice.helpers.SourceCodeRepoInterface;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +16,12 @@ public abstract class AbstractLanguageHandler {
     private static final Logger LOG = LoggerFactory.getLogger(AbstractLanguageHandler.class);
 
     /**
+     *
+     * @return  The file type that resolved imports will be automatically set to
+     */
+    protected abstract DescriptorLanguage.FileType getFileType();
+
+    /**
      * Grabs a import file from Git based on its absolute path and add to imports mapping
      * @param repositoryId              identifies the git repository that we wish to use, normally something like 'organization/repo_name`
      * @param version                   version of the files to get
@@ -27,52 +30,19 @@ public abstract class AbstractLanguageHandler {
      * @param sourceCodeRepoInterface   used too retrieve imports
      * @param absoluteImportPath        absolute path of import in git repository
      */
-    protected void handleImport(String repositoryId, Version version, Map<String, SourceFile> imports, String givenImportPath, SourceCodeRepoInterface sourceCodeRepoInterface, String absoluteImportPath, DescriptorLanguage.FileType fileType) {
-        final SourceFile sourceFile = readFile(repositoryId, version, givenImportPath, sourceCodeRepoInterface, absoluteImportPath, fileType);
-        if (sourceFile != null) {
-            imports.put(absoluteImportPath, sourceFile);
-        }
-    }
-
-    protected SourceFile readFile(String repositoryId, Version version, String givenPath, SourceCodeRepoInterface sourceCodeRepoInterface, String absolutePath, DescriptorLanguage.FileType fileType) {
-        final String fileResponse = sourceCodeRepoInterface.readGitRepositoryFile(repositoryId, fileType, version, absolutePath);
+    protected void handleImport(String repositoryId, Version version, Map<String, SourceFile> imports, String givenImportPath, SourceCodeRepoInterface sourceCodeRepoInterface, String absoluteImportPath) {
+        DescriptorLanguage.FileType fileType = getFileType();
+        // create a new source file
+        final String fileResponse = sourceCodeRepoInterface.readGitRepositoryFile(repositoryId, fileType, version, absoluteImportPath);
         if (fileResponse == null) {
-            LOG.error("Could not read file: " + absolutePath);
-            return null;
+            LOG.error("Could not read: " + absoluteImportPath);
+            return;
         }
         SourceFile sourceFile = new SourceFile();
         sourceFile.setType(fileType);
         sourceFile.setContent(fileResponse);
-        sourceFile.setPath(givenPath);
-        sourceFile.setAbsolutePath(absolutePath);
-        return sourceFile;
-    }
-
-    protected List<SourceFile> readPath(String repositoryId, Version version, String givenPath, SourceCodeRepoInterface sourceCodeRepoInterface, String absolutePath, Set<String> excludePaths, DescriptorLanguage.FileType fileType) {
-        if (excludePaths.contains(absolutePath)) {
-            return List.of();
-        }
-        SourceFile file = readFile(repositoryId, version, givenPath, sourceCodeRepoInterface, absolutePath, fileType);
-        if (file != null) {
-            return List.of(file);
-        }
-        List<String> paths = sourceCodeRepoInterface.listFiles(repositoryId, absolutePath, version.getReference());
-        if (paths != null) {
-            return readPaths(repositoryId, version, paths, sourceCodeRepoInterface, excludePaths, fileType);
-        }
-        LOG.error("Could not read path: " + absolutePath);
-        return List.of();
-    }
-
-    protected List<SourceFile> readFiles(String repositoryId, Version version, List<String> paths, SourceCodeRepoInterface sourceCodeRepoInterface, Set<String> excludePaths, DescriptorLanguage.FileType fileType) {
-        return paths.stream()
-            .filter(path -> !excludePaths.contains(path))
-            .map(path -> readFile(repositoryId, version, path, sourceCodeRepoInterface, path, fileType))
-            .filter(Objects::nonNull)
-            .toList();
-    }
-
-    protected List<SourceFile> readPaths(String repositoryId, Version version, List<String> paths, SourceCodeRepoInterface sourceCodeRepoInterface, Set<String> excludePaths, DescriptorLanguage.FileType fileType) {
-        return paths.stream().flatMap(path -> readPath(repositoryId, version, path, sourceCodeRepoInterface, path, excludePaths, fileType).stream()).toList();
+        sourceFile.setPath(givenImportPath);
+        sourceFile.setAbsolutePath(absoluteImportPath);
+        imports.put(absoluteImportPath, sourceFile);
     }
 }
