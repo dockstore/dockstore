3e5d2b3ffb99b1a21ad9edad3518298eed7350cd
Stephen Von Worley <svonworl@ucsc.edu>
2023-01-25 16:35:10 -0800

    add comments

--- dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/WorkflowSpecifications.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/WorkflowSpecifications.java
@@ -3,6 +3,17 @@ package io.dockstore.webservice.helpers;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains information used to create/update a Workflow/WorkflowVersion
+ * that is not necessarily part of those objects themselves.  An instance
+ * of this class is intended as an ephemeral vessel used to pass
+ * information between the code that parses user input (.dockstore.yml)
+ * and the workflow/version creation code.
+ *
+ * This class relates to workflows/versions as a blueprint does to a
+ * building, or a recipe to dinner: it contains information used to create
+ * the object, but it itself is not part of the object.
+ */
 public class WorkflowSpecifications {
 
     private List<String> userFilePaths = List.of();
@@ -10,10 +21,16 @@ public class WorkflowSpecifications {
     public WorkflowSpecifications() {
     }
 
+    /**
+     * Set the list of user-specified file paths.
+     */
     public void setUserFilePaths(List<String> userFilePaths) {
         this.userFilePaths = new ArrayList<>(userFilePaths);
     }
 
+    /**
+     * Get the list of user-specified file paths.
+     */
     public List<String> getUserFilePaths() {
         return userFilePaths;
     }
