4b6239c7e13e6938a5c6365e933ada7d0ddf9bf3
Stephen Von Worley <svonworl@ucsc.edu>
2023-02-01 13:38:17 -0800

    add YamlNotebook and adjust DockstoreYaml12

---
+++ dockstore-common/src/main/java/io/dockstore/common/yaml/YamlNotebook.java
@@ -0,0 +1,117 @@
+// TODO insert copyright
+
+package io.dockstore.common.yaml;
+
+import io.dockstore.common.yaml.constraints.EntryName;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+/**
+ * A notebook as described in a .dockstore.yml
+ */
+public class YamlNotebook implements Workflowish {
+
+    private String name;
+    private String format = "ipynb";
+    private String language = "python";
+    private String primaryDescriptorPath;
+    private Boolean publish = false;
+    private boolean latestTagAsDefault = false;
+    private Filters filters = new Filters();
+    private List<YamlAuthor> authors = new ArrayList<>();
+    private List<String> testParameterFiles = new ArrayList<>();
+    private List<String> otherFiles = new ArrayList<>();
+
+    @EntryName
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getSubclass() {
+        return format;
+    }
+
+    @NotNull
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @NotNull
+    public String getPrimaryDescriptorPath() {
+        return primaryDescriptorPath;
+    }
+
+    public void setPrimaryDescriptorPath(final String primaryDescriptorPath) {
+        this.primaryDescriptorPath = primaryDescriptorPath;
+    }
+
+    public Boolean getPublish() {
+        return publish;
+    }
+
+    public void setPublish(final Boolean publish) {
+        this.publish = publish;
+    }
+
+    public boolean getLatestTagAsDefault() {
+        return latestTagAsDefault;
+    }
+
+    public void setLatestTagAsDefault(boolean latestTagAsDefault) {
+        this.latestTagAsDefault = latestTagAsDefault;
+    }
+
+    @Valid
+    public Filters getFilters() {
+        return filters;
+    }
+
+    public void setFilters(final Filters filters) {
+        this.filters = filters;
+    }
+
+    @Valid
+    public List<YamlAuthor> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(final List<YamlAuthor> authors) {
+        this.authors = authors;
+    }
+
+    public List<String> getTestParameterFiles() {
+        return testParameterFiles;
+    }
+
+    public void setTestParameterFiles(final List<String> testParameterFiles) {
+        this.testParameterFiles = testParameterFiles;
+    }
+
+    @NotNull
+    public List<String> getOtherFiles() {
+        return otherFiles;
+    }
+
+    public void setOtherFiles(List<String> otherFiles) {
+        this.otherFiles = otherFiles;
+    }
+}
