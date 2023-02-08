ea98d9d55877ebbabbec1e4046fc8b89e26b2070
Stephen Von Worley <svonworl@ucsc.edu>
2023-02-08 12:51:32 -0800

    tiny refactor, add copyright header

--- dockstore-common/src/test/java/io/dockstore/common/MuteForSuccessfulTests.java
+++ dockstore-common/src/test/java/io/dockstore/common/MuteForSuccessfulTests.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 OICR and UCSC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package io.dockstore.common;
 
 import java.lang.reflect.Field;
@@ -25,11 +42,8 @@ public class MuteForSuccessfulTests implements TestWatcher {
                 // Skip any field that doesn't extend SystemStreamBase,
                 // thus avoiding a potential security/access exception on an irrelevant private field.
                 if (SystemStreamBase.class.isAssignableFrom(field.getType())) {
-                    // Make the "stream" field accessible if it is not already.
-                    if (!field.isAccessible()) {
-                        field.setAccessible(true);
-                    }
                     // Extract the "stream" field value.
+                    field.setAccessible(true);
                     if (field.get(test) instanceof SystemStreamBase stream) {
                         // Get the captured text and output it.
                         String text = stream.getText();
