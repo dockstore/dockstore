536eb58fa5fd5894a44dccdfe6d67f0301391381
Stephen Von Worley <svonworl@ucsc.edu>
2023-03-03 00:35:40 -0800

    misc tweaks

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/ImageReference.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/ImageReference.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2019 OICR
+ *    Copyright 2023 OICR and UCSC
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package io.dockstore.webservice.core;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.sql.Timestamp;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -28,28 +29,40 @@ import javax.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+/**
+ * Represents a reference to a container image.
+ * Typically, the reference would refer to a Docker image stored in an image registry, via repo/tag/id/digest.
+ * However, currently, there are no restrictions on the format of the reference.
+ * In an on-prem Dockstore install, the reference could be something unusual like a file path.
+ */
 @Entity
-@ApiModel(value = "ImageReference", description = "An image reference associated with a version")
+@ApiModel(value = "ImageReference", description = "A container image reference")
 @Table(name = "image_reference")
 @SuppressWarnings("checkstyle:magicnumber")
-
+@Schema(description = "A container image reference")
 public class ImageReference {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @ApiModelProperty(value = "Implementation specific ID for the image in this webservice", position = 0)
+    @ApiModelProperty(value = "ID of the image reference", position = 0)
+    @Schema(description = "ID of the image reference")
     private long id;
 
-    @Column()
-    @ApiModelProperty(value = "Image reference", position = 1)
+    @Column(nullable = false)
+    @ApiModelProperty(value = "Reference to an image", position = 1)
+    @Schema(description = "Reference to an image")
     private String reference;
 
-    // TODO not null
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     @CreationTimestamp
+    @ApiModelProperty(dataType = "long")
+    @Schema(type = "integer", format = "int64")
     private Timestamp dbCreateDate;
 
-    @Column()
+    @Column(nullable = false)
     @UpdateTimestamp
+    @ApiModelProperty(dataType = "long")
+    @Schema(type = "integer", format = "int64")
     private Timestamp dbUpdateDate;
 
     public ImageReference() {
