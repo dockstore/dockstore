e985347610afe4babc35d7f7fe75fa2a3d3c0d0a
Denis Yuen <denis.yuen@oicr.on.ca>
2023-03-08 13:21:15 -0500

    schema documentation

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/User.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/User.java
@@ -97,6 +97,9 @@ import org.hibernate.annotations.UpdateTimestamp;
 })
 @SuppressWarnings("checkstyle:magicnumber")
 public class User implements Principal, Comparable<User>, Serializable {
+
+    public static final String INDICATES_WHETHER_THIS_USER_IS_A_CURATOR = "Indicates whether this user is a curator";
+    public static final String INDICATES_WHETHER_THIS_ACCOUNT_CORRESPONDS_TO_A_PLATFORM_PARTNER = "Indicates whether this account corresponds to a platform partner";
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "enduser_id_seq")
     @SequenceGenerator(name = "enduser_id_seq", sequenceName = "enduser_id_seq", allocationSize = 1)
@@ -152,11 +155,13 @@ public class User implements Principal, Comparable<User>, Serializable {
     private final SortedSet<Entry> starredEntries;
 
     @Column(columnDefinition = "boolean default 'false'")
-    @ApiModelProperty(value = "Indicates whether this user is a curator", required = true, position = 11)
+    @ApiModelProperty(value = INDICATES_WHETHER_THIS_USER_IS_A_CURATOR, required = true, position = 11)
+    @Schema(description = INDICATES_WHETHER_THIS_USER_IS_A_CURATOR)
     private boolean curator;
 
     @Column(columnDefinition = "boolean default 'false'")
-    @ApiModelProperty(value = "Indicates whether this account corresponds to a platform partner", required = true, position = 18)
+    @ApiModelProperty(value = INDICATES_WHETHER_THIS_ACCOUNT_CORRESPONDS_TO_A_PLATFORM_PARTNER, required = true, position = 18)
+    @Schema(description = INDICATES_WHETHER_THIS_ACCOUNT_CORRESPONDS_TO_A_PLATFORM_PARTNER)
     private boolean platformPartner;
 
     @Column(columnDefinition = "boolean default 'false'")
