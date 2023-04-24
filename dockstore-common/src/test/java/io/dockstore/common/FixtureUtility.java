70bbe661e91cb8660e48071180275274473759de
Denis Yuen <denis.yuen@oicr.on.ca>
2023-04-24 12:10:02 -0400

    fixture fix

--- dockstore-common/src/test/java/io/dockstore/common/FixtureUtility.java
+++ dockstore-common/src/test/java/io/dockstore/common/FixtureUtility.java
@@ -16,24 +16,76 @@
 
 package io.dockstore.common;
 
-import io.dropwizard.testing.ResourceHelpers;
-import java.io.File;
+import com.google.common.io.ByteStreams;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import org.apache.commons.io.FileUtils;
 
+/**
+ * All copied from dropwizard 4 due to deletion in dropwizard 5.
+ *
+ * <a href="https://github.com/dropwizard/dropwizard/blob/9f736f6732373d559626f7dd5bb0a66b5bd94493/dropwizard-testing/src/main/java/io/dropwizard/testing/FixtureHelpers.java">...</a>
+ * <a href="https://github.com/dropwizard/dropwizard/blob/9f736f6732373d559626f7dd5bb0a66b5bd94493/dropwizard-util/src/main/java/io/dropwizard/util/Resources.java">...</a>
+ */
 public final class FixtureUtility {
-
     private FixtureUtility() {
         //  block construction
     }
 
-    public static String fixture(String path) {
-        try {
-            return FileUtils.readFileToString(new File(ResourceHelpers.resourceFilePath(path)), StandardCharsets.UTF_8);
+    /**
+     * Reads the given fixture file from the classpath (e. g. {@code src/test/resources})
+     * and returns its contents as a UTF-8 string.
+     *
+     * @param filename the filename of the fixture file
+     * @return the contents of {@code src/test/resources/{filename}}
+     * @throws IllegalArgumentException if an I/O error occurs.
+     */
+    public static String fixture(String filename) {
+        return fixture(filename, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * Reads the given fixture file from the classpath (e. g. {@code src/test/resources})
+     * and returns its contents as a string.
+     *
+     * @param filename the filename of the fixture file
+     * @param charset  the character set of {@code filename}
+     * @return the contents of {@code src/test/resources/{filename}}
+     * @throws IllegalArgumentException if an I/O error occurs.
+     */
+    private static String fixture(String filename, Charset charset) {
+        final URL resource = getResource(filename);
+        try (InputStream inputStream = resource.openStream()) {
+            return new String(ByteStreams.toByteArray(inputStream), charset).trim();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new IllegalArgumentException(e);
         }
+    }
+
+    /**
+     * Returns a {@code URL} pointing to {@code resourceName} if the resource is found using the
+     * {@linkplain Thread#getContextClassLoader() context class loader}. In simple environments, the
+     * context class loader will find resources from the class path. In environments where different
+     * threads can have different class loaders, for example app servers, the context class loader
+     * will typically have been set to an appropriate loader for the current thread.
+     *
+     * <p>In the unusual case where the context class loader is null, the class loader that loaded
+     * this class ({@code Resources}) will be used instead.
+     *
+     * @param resourceName the name of the resource
+     * @return the {@link URL} of the resource
+     * @throws IllegalArgumentException if the resource is not found
+     */
+    public static URL getResource(String resourceName) {
+        final ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        final ClassLoader loader = contextClassLoader == null ? FixtureUtility.class.getClassLoader() : contextClassLoader;
+        final URL url = loader.getResource(resourceName);
+        if (url == null) {
+            throw new IllegalArgumentException("resource " + resourceName + " not found.");
+        }
+        return url;
     }
 
 }
