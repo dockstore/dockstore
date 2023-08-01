5b5600a10a9955bb688099a957205c54b1ae965f
Denis Yuen <denis.yuen@oicr.on.ca>
2023-08-01 10:47:40 -0400

    cross registry test

--- dockstore-integration-testing/src/test/java/io/dockstore/webservice/SubmoduleIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/webservice/SubmoduleIT.java
@@ -1,5 +1,6 @@
 package io.dockstore.webservice;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.dockstore.client.cli.BaseIT;
@@ -9,10 +10,13 @@ import io.dockstore.common.CommonTestUtilities;
 import io.dockstore.common.ConfidentialTest;
 import io.dockstore.common.MuteForSuccessfulTests;
 import io.dockstore.openapi.client.ApiClient;
+import io.dockstore.openapi.client.api.LambdaEventsApi;
 import io.dockstore.openapi.client.api.WorkflowsApi;
+import io.dockstore.openapi.client.model.LambdaEvent;
 import io.dockstore.openapi.client.model.SourceFile;
 import io.dockstore.openapi.client.model.Workflow;
 import io.dockstore.openapi.client.model.WorkflowSubClass;
+import io.dockstore.openapi.client.model.WorkflowVersion;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -54,6 +58,7 @@ class SubmoduleIT extends BaseIT {
 
         Workflow foobar = workflowClient.getWorkflowByPath("github.com/" + workflowDockstoreYmlRepo + "/wdl-humanwgs", WorkflowSubClass.BIOWORKFLOW, "versions");
         final List<SourceFile> sourcefiles = workflowClient.getWorkflowVersionsSourcefiles(foobar.getId(), foobar.getWorkflowVersions().get(0).getId(), null);
+        assertTrue(foobar.getWorkflowVersions().stream().allMatch(WorkflowVersion::isValid));
         // these files are in a different repo entirely
         assertTrue(sourcefiles.stream().anyMatch(f -> f.getPath().contains("../wdl-common/wdl/workflows/phase_vcf/phase_vcf.wdl")));
         assertTrue(sourcefiles.stream().anyMatch(f -> f.getPath().contains("../wdl-common/wdl/workflows/deepvariant/deepvariant.wdl")));
@@ -70,6 +75,7 @@ class SubmoduleIT extends BaseIT {
         workflowClient.handleGitHubRelease("refs/tags/v0.11-submodule-in-submodule", installationId, workflowDockstoreYmlRepo, BasicIT.USER_2_USERNAME);
 
         Workflow foobar = workflowClient.getWorkflowByPath("github.com/" + workflowDockstoreYmlRepo + "/wdl-humanwgs", WorkflowSubClass.BIOWORKFLOW, "versions");
+        assertTrue(foobar.getWorkflowVersions().stream().allMatch(WorkflowVersion::isValid));
         final List<SourceFile> sourcefiles = workflowClient.getWorkflowVersionsSourcefiles(foobar.getId(), foobar.getWorkflowVersions().get(0).getId(), null);
         // these files are in a different repo entirely
         assertTrue(sourcefiles.stream().anyMatch(f -> f.getPath().contains("../wdl-common/wdl/workflows/phase_vcf/phase_vcf.wdl")));
@@ -77,5 +83,26 @@ class SubmoduleIT extends BaseIT {
         // this one is inside a different repo which points to yet a different repo
         assertTrue(sourcefiles.stream().anyMatch(f -> f.getPath().contains("../wdl-common/wdl/tasks/zip_index_vcf.wdl")));
         assertTrue(sourcefiles.size() == 19);
+    }
+
+    @Test
+    void testSubmodulesCrossRegistryCase() {
+        final ApiClient webClient = getOpenAPIWebClient(BasicIT.USER_2_USERNAME, testingPostgres);
+        WorkflowsApi workflowClient = new WorkflowsApi(webClient);
+
+        // this tag has a bitbucket submodule, turns out even GitHub doesn't work well with it https://github.com/dockstore-testing/wdl-humanwgs/tree/v0.11-bitbucket-submodule/workflows
+        // GitHub actually links to a 404
+        workflowClient.handleGitHubRelease("refs/tags/v0.11-bitbucket-submodule", installationId, workflowDockstoreYmlRepo, BasicIT.USER_2_USERNAME);
+
+        Workflow foobar = workflowClient.getWorkflowByPath("github.com/" + workflowDockstoreYmlRepo + "/wdl-humanwgs", WorkflowSubClass.BIOWORKFLOW, "versions");
+        assertTrue(foobar.getWorkflowVersions().stream().noneMatch(WorkflowVersion::isValid));
+        final List<SourceFile> sourcefiles = workflowClient.getWorkflowVersionsSourcefiles(foobar.getId(), foobar.getWorkflowVersions().get(0).getId(), null);
+        // these files are not present
+        assertFalse(sourcefiles.stream().anyMatch(f -> f.getPath().contains("../wdl-common/wdl/workflows/phase_vcf/phase_vcf.wdl")));
+        assertFalse(sourcefiles.stream().anyMatch(f -> f.getPath().contains("../wdl-common/wdl/workflows/deepvariant/deepvariant.wdl")));
+        assertFalse(sourcefiles.stream().anyMatch(f -> f.getPath().contains("../wdl-common/wdl/tasks/zip_index_vcf.wdl")));
+        LambdaEventsApi lambdaEventsApi = new io.dockstore.openapi.client.api.LambdaEventsApi(webClient);
+        final List<LambdaEvent> lambdaEventsByOrganization = lambdaEventsApi.getLambdaEventsByOrganization("dockstore-testing", "0", 10);
+        assertTrue(lambdaEventsByOrganization.stream().anyMatch(e -> e.getMessage().contains("Failed to import") && e.getMessage().contains("Not found wdl-common/wdl")));
     }
 }
