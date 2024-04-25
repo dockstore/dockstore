433929e888e32a61a0f35e1ba30231b0f91a22f5
hyunnaye <nayeonhyun802@gmail.com>
2024-04-25 13:25:38 -0400

    collectionentry exception

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/CollectionEntry.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/CollectionEntry.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dockstore.common.SourceControl;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.io.InvalidObjectException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,7 +27,9 @@ public class CollectionEntry implements Serializable {
     @JsonProperty("categories")
     private List<CategorySummary> categorySummaries = new ArrayList<>();
 
-    public CollectionEntry() {}
+    public CollectionEntry() throws InvalidObjectException {
+        throw new InvalidObjectException("Invalid CollectionEntry");
+    }
 
     /**
      * @param id
