07da38bfd5aa2951ab37ca68f4eb3f04b36ee13c
Stephen Von Worley <svonworl@ucsc.edu>
2024-05-28 16:43:30 -0700

    tiny refactor

--- dockstore-webservice/src/test/java/io/dockstore/webservice/core/LimitedSourceFileBuilderTest.java
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/core/LimitedSourceFileBuilderTest.java
@@ -4,14 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.dockstore.common.DescriptorLanguage;
-import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class LimitedSourceFileBuilderTest {
 
     private static final DescriptorLanguage.FileType TYPE = DescriptorLanguage.FileType.DOCKSTORE_WDL;
     private static final String SMALL_CONTENT = "This is some content.";
-    private static final String BIG_CONTENT = ".".repeat(100_000_000);
+    private static final String HUGE_CONTENT = ".".repeat(100_000_000);
     private static final String BINARY_CONTENT = "Content that contains a nul character \u0000";
     private static final String PATH = "abc.wdl";
     private static final String ABSOLUTE_PATH = "/abc.wdl";
@@ -26,8 +25,8 @@ class LimitedSourceFileBuilderTest {
     }
 
     @Test
-    void testBigContent() {
-        SourceFile file = SourceFile.limitedBuilder().type(TYPE).content(BIG_CONTENT).path(PATH).absolutePath(ABSOLUTE_PATH).build();
+    void testHugeContent() {
+        SourceFile file = SourceFile.limitedBuilder().type(TYPE).content(HUGE_CONTENT).path(PATH).absolutePath(ABSOLUTE_PATH).build();
         assertEquals(TYPE, file.getType());
         assertTrue(file.getContent().startsWith("Dockstore does not store files of this type over"));
         assertEquals(PATH, file.getPath());
