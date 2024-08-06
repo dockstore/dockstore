04866e5e0777faac8243084f754f1935831e68d9
Denis Yuen <denis.yuen@oicr.on.ca>
2024-08-06 15:36:52 -0400

    need sudo

--- scripts/install-tests.sh
+++ scripts/install-tests.sh
@@ -14,10 +14,10 @@ if [ "${TESTING_PROFILE}" = "regression-integration-tests" ]; then
     pip3 install -r dockstore-webservice/src/main/resources/requirements/1.13.0/requirements3.txt
 elif [ "${TESTING_PROFILE}" == "language-parsing-tests" ]; then
     #pip3 install -r dockstore-webservice/src/main/resources/requirements/1.14.0/requirements3.txt
-    apt-get update
+    sudo apt-get update
     # https://stackoverflow.com/questions/44331836/apt-get-install-tzdata-noninteractive needed by cwltool
-    DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
-    apt-get -qq --yes --force-yes install cwltool
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install -y tzdata
+    sudo apt-get -qq --yes --force-yes install cwltool
 else
     pip3 install --user -r dockstore-webservice/src/main/resources/requirements/1.14.0/requirements3.txt
 fi
