5a881aba9cea97ff5a0697bcf3355d1cedbf30e9
Charles Overbeck <coverbec@ucsc.edu>
2024-08-28 16:55:31 -0700

    D'oh, bis

--- dockstore-integration-testing/src/test/java/io/dockstore/webservice/ZenodoIT.java
+++ dockstore-integration-testing/src/test/java/io/dockstore/webservice/ZenodoIT.java
@@ -422,6 +422,7 @@ class ZenodoIT {
         // is already a test for that.
         testingPostgres.runUpdateStatement("update workflow set ispublished = true, waseverpublic = true;");
 
+        workflowsApi.updateDois(null);
         final List<Workflow> workflows = workflowsApi.getAllWorkflowByPath("github.com/" + DockstoreTesting.WORKFLOW_DOCKSTORE_YML);
         workflows.forEach(workflow -> assertNotNull(workflow.getConceptDois().get(DoiInitiator.GITHUB)));
 
