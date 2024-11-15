fcf2180e4bc4569fbce1cfc0c1c8861f07cab701
Stephen Von Worley <svonworl@ucsc.edu>
2024-11-15 13:35:54 -0800

    add copyright notice, refactor

--- dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/ExceptionHelper.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/ExceptionHelper.java
@@ -1,4 +1,20 @@
-// TODO header
+/*
+ * Copyright 2024 OICR and UCSC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package io.dockstore.webservice.helpers;
 
 import jakarta.persistence.PersistenceException;
@@ -84,11 +100,15 @@ public class ExceptionHelper {
     }
 
     private Optional<Info> result(int status) {
-        return result(throwable.getMessage(), status);
+        return result(defaultMessage(), status);
     }
 
     private Optional<Info> result(String message, int status) {
         return Optional.of(new Info(message, status));
+    }
+
+    private String defaultMessage() {
+        return throwable.getMessage();
     }
 
     public record Info(String message, int status) {
