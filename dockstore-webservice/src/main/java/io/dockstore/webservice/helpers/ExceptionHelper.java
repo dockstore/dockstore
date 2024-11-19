46ffbed4658335f138405477d7ac69019be5cacd
Stephen Von Worley <svonworl@ucsc.edu>
2024-11-18 17:14:52 -0800

    improve performance, add tests

--- dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/ExceptionHelper.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/helpers/ExceptionHelper.java
@@ -18,9 +18,10 @@
 package io.dockstore.webservice.helpers;
 
 import jakarta.persistence.PersistenceException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.http.HttpStatus;
 import org.hibernate.exception.ConstraintViolationException;
 
@@ -35,8 +36,17 @@ public class ExceptionHelper {
         this.throwables = listThrowables(throwable);
     }
 
+    // This is an improved implementation of ExceptionUtils.getThrowableList():
+    // https://github.com/apache/commons-lang/blob/0fde05172e853c3dff55d1841ad21c8cce363259/src/main/java/org/apache/commons/lang3/exception/ExceptionUtils.java#L517-L524
+    // The original has O(N^2) runtime on average, where N is the number of throwables, which could be used as an attack.
+    @SuppressWarnings("checkstyle:IllegalType")
     private static List<Throwable> listThrowables(Throwable throwable) {
-        return ExceptionUtils.getThrowableList(throwable);
+        final LinkedHashSet<Throwable> list = new LinkedHashSet<>();
+        while (throwable != null && !list.contains(throwable)) {
+            list.add(throwable);
+            throwable = throwable.getCause();
+        }
+        return new ArrayList<>(list);
     }
 
     public Info info() {
@@ -55,12 +65,12 @@ public class ExceptionHelper {
         return info().status();
     }
 
-    private <T extends Throwable> Optional<T> cause(Class<T> klass) {
+    private <T extends Throwable> Optional<T> findThrowable(Class<T> klass) {
         return throwables.stream().filter(klass::isInstance).map(klass::cast).findFirst();
     }
 
-    private <T extends Throwable> boolean hasCause(Class<T> klass) {
-        return cause(klass).isPresent();
+    private <T extends Throwable> boolean hasThrowable(Class<T> klass) {
+        return findThrowable(klass).isPresent();
     }
 
     private Optional<Info> handleJavaThrowable() {
@@ -71,7 +81,7 @@ public class ExceptionHelper {
     }
 
     private Optional<Info> handleConstraintViolationException() {
-        return cause(ConstraintViolationException.class).flatMap(this::mapConstraintViolationException);
+        return findThrowable(ConstraintViolationException.class).flatMap(this::mapConstraintViolationException);
     }
 
     private Optional<Info> mapConstraintViolationException(ConstraintViolationException c) {
@@ -84,17 +94,26 @@ public class ExceptionHelper {
         return name == null
             ? "a database constraint was violated"
             : switch (name.toLowerCase()) {
-                case "tool_toolname_check" -> "a tool with the same name already exists";
-                case "workflow_check" -> "an entry with the same name already exists";
-                case "unique_tag_names" -> "a tag with the same name already exists";
-                case "unique_workflowversion_names" -> "a version with the same name already exists";
-                case "unique_doi_name" -> "a DOI with the same name already exists";
-                default -> " database constraint '%s' was violated".formatted(name);
+                case "aliases_are_unique" -> same("an entry", "alias");
+                case "check_valid_doi" -> "the DOI is not valid";
+                case "check_valid_orcid" -> "the ORCID is not valid";
+                case "tool_toolname_check" -> same("a tool", "name");
+                case "unique_col_aliases" -> same("a collection", "alias");
+                case "unique_doi_name" -> same("a DOI", "name");
+                case "unique_org_aliases" -> same("an organization", "alias");
+                case "unique_tag_names" -> same("a version", "name");
+                case "unique_workflowversion_names" -> same("a version", "name");
+                case "username_unique" -> same("a user", "name");
+                default -> "database constraint '%s' was violated".formatted(name);
             };
     }
 
+    private String same(String subject, String attribute) {
+        return "%s with the same %s already exists".formatted(subject, attribute);
+    }
+
     private Optional<Info> handlePersistenceException() {
-        return hasCause(PersistenceException.class)
+        return hasThrowable(PersistenceException.class)
             ? result("the database could not be updated", HttpStatus.SC_INTERNAL_SERVER_ERROR)
             : NONE;
     }
--- dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/ExceptionHelperTest.java
+++ dockstore-webservice/src/test/java/io/dockstore/webservice/helpers/ExceptionHelperTest.java
@@ -1,11 +1,13 @@
 package io.dockstore.webservice.helpers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import jakarta.persistence.PersistenceException;
 import org.hibernate.TransactionException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 class ExceptionHelperTest {
 
@@ -19,5 +21,100 @@ class ExceptionHelperTest {
         // Confirm the subclass relationship:
         assertTrue(PersistenceException.class.isAssignableFrom(TransactionException.class));
         assertTrue(PersistenceException.class.isAssignableFrom(ConstraintViolationException.class));
+    }
+
+    @Test
+    void testUnknownException() {
+        String message = "this is a test";
+        Throwable t = makeNonJavaException(message);
+        assertEquals(t.getMessage(), message(t));
+        assertEquals(400, status(t));
+    }
+
+    @Test
+    void testJavaThrowable() {
+        Throwable t = makeNullPointerException();
+        assertEquals(t.getMessage(), message(t));
+        assertEquals(500, status(t));
+    }
+
+    @Test
+    void testConstraintViolationException() {
+        Throwable t = makeConstraintViolationException("xyz_123");
+        assertTrue(contains(message(t), "constraint", "violated", "xyz_123"));
+        assertEquals(409, status(t));
+
+        t = wrap(t, "something went wrong");
+        assertTrue(contains(message(t), "constraint", "violated", "xyz_123"));
+        assertEquals(409, status(t));
+
+        t = makeConstraintViolationException("check_valid_doi");
+        assertTrue(contains(message(t), "doi", "valid"));
+        assertEquals(409, status(t));
+
+        t = makeConstraintViolationException(null);
+        assertTrue(contains(message(t), "constraint", "violated"));
+        assertEquals(409, status(t));
+    }
+
+    @Test
+    void testPersistenceException() {
+        Throwable t = makePersistenceException();
+        assertTrue(contains(message(t), "database", "updated"));
+        assertEquals(500, status(t));
+    }
+
+    @Test
+    @Timeout(5)
+    void testVeryLargeNumberOfCauses() {
+        Throwable t = new RuntimeException();
+        for (int i = 0; i < 100000; i++) {
+            t = new RuntimeException("wrapper", t);
+        }
+        message(t);
+        status(t);
+    }
+
+    private Throwable makeNonJavaException(String message) {
+        return new RuntimeException(message) {};
+    }
+
+    private Throwable wrap(Throwable t, String message) {
+        return new RuntimeException(message, t) {};
+    }
+
+    private NullPointerException makeNullPointerException() {
+        try {
+            Object o = null;
+            o.hashCode();
+        } catch (NullPointerException e) {
+            return e;
+        }
+        throw new RuntimeException("should not be reachable");
+    }
+
+    private PersistenceException makePersistenceException() {
+        return new PersistenceException();
+    }
+
+    private ConstraintViolationException makeConstraintViolationException(String constraintName) {
+        return new ConstraintViolationException("oops", null, constraintName);
+    }
+
+    private String message(Throwable t) {
+        return new ExceptionHelper(t).info().message();
+    }
+
+    private int status(Throwable t) {
+        return new ExceptionHelper(t).info().status();
+    }
+
+    private boolean contains(String s, String... substrings) {
+        for (String substring: substrings) {
+            if (!s.toLowerCase().contains(substring.toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
