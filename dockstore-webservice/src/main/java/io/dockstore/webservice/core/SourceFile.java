ba799d93ba2f6892bec249f5e30e55b5422afccb
Denis Yuen <denis.yuen@oicr.on.ca>
2025-04-28 17:12:22 -0400

    further sanitize sourcefiles, strict

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/SourceFile.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/SourceFile.java
@@ -17,6 +17,10 @@
 package io.dockstore.webservice.core;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ComparisonChain;
 import com.google.gson.ExclusionStrategy;
@@ -53,6 +57,7 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotNull;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -66,6 +71,8 @@ import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -126,6 +133,7 @@ public class SourceFile implements Comparable<SourceFile> {
 
     @Column(columnDefinition = "TEXT")
     @ApiModelProperty(value = "Cache for the contents of the target file", position = 2)
+    @JsonSerialize(using = UserStringToStringSerializer.class, as = String.class)
     private String content;
 
     @Column(nullable = false, columnDefinition = "TEXT")
@@ -400,5 +408,16 @@ public class SourceFile implements Comparable<SourceFile> {
          * The file represents a stub.  The content field is null.
          */
         STUB
+    }
+
+    public static class UserStringToStringSerializer extends JsonSerializer<String> {
+
+        @Override
+        public void serialize(String string,
+            JsonGenerator jsonGenerator,
+            SerializerProvider serializerProvider)
+            throws IOException {
+            jsonGenerator.writeObject(Jsoup.clean(string, Safelist.none()));
+        }
     }
 }
