7d766dbddfd147d9a6415b53d2889e4f804b0076
Stephen Von Worley <svonworl@ucsc.edu>
2026-06-22 15:02:34 -0700

    add metadata to CategorySummary

--- dockstore-webservice/src/main/java/io/dockstore/webservice/core/CategorySummary.java
+++ dockstore-webservice/src/main/java/io/dockstore/webservice/core/CategorySummary.java
@@ -35,8 +35,9 @@ public class CategorySummary implements Serializable {
     private String topic;
     private EntryVersion.Curator curator;
     private boolean aiManaged;
+    private Map<String, String> metadata;
 
-    public CategorySummary(long id, String name, String description, String displayName, String topic, EntryVersion.Curator curator, boolean aiManaged) {
+    public CategorySummary(long id, String name, String description, String displayName, String topic, EntryVersion.Curator curator, boolean aiManaged, Map<String, String> metadata) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -44,6 +45,7 @@ public class CategorySummary implements Serializable {
         this.topic = topic;
         this.curator = curator;
         this.aiManaged = aiManaged;
+        this.metadata = metadata;
     }
 
     public long getId() {
@@ -63,7 +65,7 @@ public class CategorySummary implements Serializable {
     }
 
     public String getTopic() {
-        return (topic);
+        return topic;
     }
 
     public EntryVersion.Curator getCurator() {
@@ -72,5 +74,9 @@ public class CategorySummary implements Serializable {
 
     public boolean isAiManaged() {
         return aiManaged;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
     }
 }
